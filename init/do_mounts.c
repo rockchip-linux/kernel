a7e0e1586a53716d8173f9aeb2b3393c1610344a
Arjan van de Ven <arjan@linux.intel.com>
2008-07-20 13:12:16 -0700

    MOBLIN: fastboot: remove "wait for all devices before mounting root" delay
    In the non-initrd case, we wait for all devices to finish their
    probing before we try to mount the rootfs.
    In practice, this means that we end up waiting 2 extra seconds for
    the PS/2 mouse probing even though the root holding device has been
    ready since a long time.
    
    The previous two patches in this series made the RAID autodetect code
    do it's own "wait for probing to be done" code, and added
    "wait and retry" functionality in case the root device isn't actually
    available.
    
    These two changes should make it safe to remove the delay itself,
    and this patch does this. On my test laptop, this reduces the boot time
    by 2 seconds (kernel time goes from 3.9 to 1.9 seconds).
    
    Change-Id: Ie32e9d5f93b35b1aed0d955e8e472b9fc73227e4
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Mandeep Singh Baines <msb@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/1667
    Tested-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -545,6 +545,7 @@ void __init prepare_namespace(void)
 		ssleep(root_delay);
 	}
 
+#if 0
 	/*
 	 * wait for the known devices to complete their probing
 	 *
@@ -553,6 +554,8 @@ void __init prepare_namespace(void)
 	 * for the touchpad of a laptop to initialize.
 	 */
 	wait_for_device_probe();
+#endif
+	async_synchronize_full();
 
 	md_run_setup();
 
