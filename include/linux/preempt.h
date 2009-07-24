d9417089a1c641216a506ba0eeb7b8887cefc932
Thomas Gleixner <tglx@linutronix.de>
2009-07-24 12:38:56 +0200

    preempt: Provide preempt_*_(no)rt variants
    RT needs a few preempt_disable/enable points which are not necessary
    otherwise. Implement variants to avoid #ifdeffery.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/preempt.h
+++ include/linux/preempt.h
@@ -185,7 +185,11 @@ do { \
 	preempt_count_dec(); \
 } while (0)
 
-#define preempt_enable_no_resched() sched_preempt_enable_no_resched()
+#ifdef CONFIG_PREEMPT_RT
+# define preempt_enable_no_resched() sched_preempt_enable_no_resched()
+#else
+# define preempt_enable_no_resched() preempt_enable()
+#endif
 
 #define preemptible()	(preempt_count() == 0 && !irqs_disabled())
 
@@ -278,6 +282,18 @@ do { \
 	if (tif_need_resched()) \
 		set_preempt_need_resched(); \
 } while (0)
+
+#ifdef CONFIG_PREEMPT_RT
+# define preempt_disable_rt()		preempt_disable()
+# define preempt_enable_rt()		preempt_enable()
+# define preempt_disable_nort()		barrier()
+# define preempt_enable_nort()		barrier()
+#else
+# define preempt_disable_rt()		barrier()
+# define preempt_enable_rt()		barrier()
+# define preempt_disable_nort()		preempt_disable()
+# define preempt_enable_nort()		preempt_enable()
+#endif
 
 #ifdef CONFIG_PREEMPT_NOTIFIERS
 
