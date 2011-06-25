42ac2666d41a7242288bce8d9a88722c65a1bb74
Thomas Gleixner <tglx@linutronix.de>
2011-06-25 09:21:04 +0200

    sched: Add saved_state for tasks blocked on sleeping locks
    Spinlocks are state preserving in !RT. RT changes the state when a
    task gets blocked on a lock. So we need to remember the state before
    the lock contention. If a regular wakeup (not a RTmutex related
    wakeup) happens, the saved_state is updated to running. When the lock
    sleep is done, the saved state is restored.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1377,6 +1377,7 @@ struct tlbflush_unmap_batch {
 
 struct task_struct {
 	volatile long state;	/* -1 unrunnable, 0 runnable, >0 stopped */
+	volatile long saved_state;	/* saved state for "spinlock sleepers" */
 	void *stack;
 	atomic_t usage;
 	unsigned int flags;	/* per process flags, defined below */
@@ -2480,6 +2481,7 @@ extern void xtime_update(unsigned long ticks);
 
 extern int wake_up_state(struct task_struct *tsk, unsigned int state);
 extern int wake_up_process(struct task_struct *tsk);
+extern int wake_up_lock_sleeper(struct task_struct * tsk);
 extern void wake_up_new_task(struct task_struct *tsk);
 #ifdef CONFIG_SMP
  extern void kick_process(struct task_struct *tsk);
