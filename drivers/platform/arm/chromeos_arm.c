d4bdb57b296b79b6738afd6e7b34a9114b6ffe1f
Olof Johansson <olofj@chromium.org>
2011-07-07 10:25:17 -0700

    CHROMIUM: chromeos_arm: use device tree
    This change removes the old shared memory model, and uses the device
    tree to pass the boot-time variables directly to userspace. Left to
    handle is just the in-kernel use of being able to read and write the
    "nvram" block on MMC to mark that the system needs recovery, etc.
    
    Also, remove the locally-cashed copy of the nonvolatile storage from
    the driver, since userspace does direct modification of the block by
    opening the device and reading/writing from it. If a local copy is kept,
    it ends up getting out of sync with the state set from userspace.
    
    This requires a bit of trickery with flushing the buffer cache to avoid
    the userspace tool from getting stale data (and vice versa).
    
    BUG=chromium-os:16665
    TEST=boot with vboot on and off, check for function
    
    Change-Id: Id374cf42088ffe86c65b126412b92441d5aa1095
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/3766
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>

--- drivers/platform/arm/chromeos_arm.c
+++ drivers/platform/arm/chromeos_arm.c
@@ -19,84 +19,25 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <asm/io.h>
-#include <asm/setup.h>
+#define pr_fmt(fmt) "chromeos_arm: " fmt
+
 #include <linux/chromeos_platform.h>
-#include <linux/debugfs.h>
-#include <linux/module.h>
-#include <linux/seq_file.h>
 #include <linux/ide.h>
+#include <linux/of.h>
+
+
 #include "../chromeos.h"
 
 /* TODO:
- * replace the shared memory block with FDT
- * or structured ATAGS
+ * Do a proper search for the right mmc device to use
  */
 
-#define FIRMWARE_SHARED_PHYSICAL_SIZE (1024 * 1024)
 #define BLKNV_MAJOR MMC_BLOCK_MAJOR
 #define BLKNV_MINOR 0
-#define MODULE_NAME "chromeos_arm"
-#define SHARED_MEM_VERSION 1
-
-/*
- * This structure must match the head of the structure in
- * u-boot-next:files/lib/chromeos/os_storage.c
- */
-struct shared_data {
-	u32 total_size;
-	u8 signature[10];
-	u16 version;
-	u64 nvcxt_lba;
-	u16 vbnv[2];
-	u8  nvcxt_cache[1]; /* the actual size is in vbnv[1] */
-} __attribute__((packed));
-
-static struct dentry *debugfs_entry;
-static struct shared_data *firmware_shared_data;
-static u64 phys_window_base = 0x3ff00000;
 
 /* location where the nvram data blends into the sector on the MMC device */
 static u16 nv_offset, nv_size;
-
-static int verify_shared_memory(void)
-{
-	const char signature[] = "CHROMEOS";
-
-	if (firmware_shared_data->version != SHARED_MEM_VERSION) {
-		pr_err(MODULE_NAME ": version mismatch: %d != %d\n",
-		       firmware_shared_data->version, SHARED_MEM_VERSION);
-		return -EINVAL;
-	}
-
-	if(memcmp(firmware_shared_data->signature, signature,
-		  sizeof(signature))) {
-		pr_err(MODULE_NAME ": signature mismatch\n");
-		return -EINVAL;
-	}
-
-	if ((nv_offset > SECTOR_SIZE) ||
-	    !nv_size ||
-	    ((nv_offset + nv_size) > SECTOR_SIZE) ||
-	    (nv_size > MAX_NVRAM_BUFFER_SIZE)) {
-		/* nvram block won't fit into a sector */
-		pr_err(MODULE_NAME ": bad nvram location: %d:%d!\n",
-		       nv_offset, nv_size);
-		return -EINVAL;
-	}
-	return 0;
-}
-
-static int chromeos_arm_show(struct seq_file *s, void *unused)
-{
-	seq_write(s, firmware_shared_data, firmware_shared_data->total_size);
-	return 0;
-}
-
-static int debugfs_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, chromeos_arm_show, inode->i_private);
-}
+static u64 nv_lba;
 
 /*
  * Functions to support nvram on block device. The actual device used is minor
@@ -106,8 +47,9 @@ static int debugfs_open(struct inode *inode, struct file *file)
  */
 static void blknv_endio(struct bio *bio, int err)
 {
-	complete((struct completion *)bio->bi_private);
+	struct completion *c = bio->bi_private;
 	bio->bi_private = (void *)err;
+	complete(c);
 }
 
 static void blknv_submit_bio(struct bio *bio, int rq)
@@ -133,7 +75,7 @@ static int chromeos_access_nvram_block(struct page *page,
 	mdev = MKDEV(BLKNV_MAJOR, BLKNV_MINOR);
 	bdev = blkdev_get_by_dev(mdev, devmode, NULL);
 	if (IS_ERR(bdev)) {
-		pr_err(MODULE_NAME ": could not open dev=[%d:%d]\n",
+		pr_err("could not open dev=[%d:%d]\n",
 		       BLKNV_MAJOR, BLKNV_MINOR);
 		ret = -EFAULT;
 		goto out;
@@ -160,13 +102,16 @@ static int chromeos_access_nvram_block(struct page *page,
 		rq |= REQ_WRITE;
 
 	blknv_submit_bio(bio, rq);
-	if (bio->bi_private)
-		ret = (int)bio->bi_private;
-	else
-		ret = 0;
+
+	/* nvblk_endio passes up any error in bi_private */
+	ret = (int)bio->bi_private;
 out:
 	if (bio)
 		bio_put(bio);
+	if (!is_read) {
+		fsync_bdev(bdev);
+		invalidate_bdev(bdev);
+	}
 	if (bdev)
 		blkdev_put(bdev, devmode);
 	return ret;
@@ -182,47 +127,69 @@ static int chromeos_write_nvram_block(struct page *page, sector_t sector)
 	return chromeos_access_nvram_block(page, sector, 0);
 }
 
-
 /*
  * This function accepts a buffer with exactly nv_size bytes. It reads the
- * appropriate mmc one sector block, blends in the new nvram contents and then
- * writes the sector back. If successful, the cached nvram in the shared
- * memory is also updated.
+ * appropriate mmc one sector block, extacts the nonvolatile data from there
+ * and copies it to the provided buffer.
  */
-static int _chromeos_write(const u8 *data)
+static int _chromeos_read(u8 *data)
 {
 	struct page *page;
 	char *virtual_addr;
-	int rv;
+	int ret;
 
 	page = alloc_page(GFP_NOIO);
 	if (!page) {
-		pr_err(MODULE_NAME ": page allocation failed\n");
+		pr_err("page allocation failed\n");
 		return -ENOMEM;
 	}
 
 	virtual_addr = page_address(page);
 	if (!virtual_addr) {
-		pr_err(MODULE_NAME ": page not mapped!\n");
+		pr_err("page not mapped!\n");
 		__free_page(page);
 		return -EFAULT;
 	}
 
-	rv = chromeos_read_nvram_block(page, firmware_shared_data->nvcxt_lba);
-	if (rv)
-		goto unwind;
+	ret = chromeos_read_nvram_block(page, nv_lba);
+	if (ret)
+		goto out;
 
-	/* As a sanity check, let's confirm that what we read is indeed the
-	 * sector containing the NVRAM. Note that on a running system the
-	 * NVRAM block will include its CRC.
-	 */
-	if (memcmp(virtual_addr + nv_offset,
-		   firmware_shared_data->nvcxt_cache, nv_size)) {
-		pr_err(MODULE_NAME ": cached contents mismatch!\n");
-		rv = -EFAULT;
-		goto unwind;
+	memcpy(data, virtual_addr + nv_offset, nv_size);
+	ret = nv_size;
+
+out:
+	__free_page(page);
+	return ret;
+}
+
+/*
+ * This function accepts a buffer with exactly nv_size bytes. It reads the
+ * appropriate mmc one sector block, blends in the new nvram contents and then
+ * writes the sector back.
+ */
+static int _chromeos_write(const u8 *data)
+{
+	struct page *page;
+	char *virtual_addr;
+	int ret;
+
+	page = alloc_page(GFP_NOIO);
+	if (!page) {
+		pr_err("page allocation failed\n");
+		return -ENOMEM;
 	}
 
+	virtual_addr = page_address(page);
+	if (!virtual_addr) {
+		pr_err("page not mapped!\n");
+		__free_page(page);
+		return -EFAULT;
+	}
+
+	ret = chromeos_read_nvram_block(page, nv_lba);
+	if (ret)
+		goto out;
 
 	/*
 	 * Sector has been read, lets blend in nvram data and write the sector
@@ -230,164 +197,130 @@ static int _chromeos_write(const u8 *data)
 	 */
 	memcpy(virtual_addr + nv_offset, data, nv_size);
 
-	rv = chromeos_write_nvram_block(page, firmware_shared_data->nvcxt_lba);
-	if (!rv) {
-		/* Write was successful, now update the local cache */
-		memcpy(firmware_shared_data->nvcxt_cache,
-		       virtual_addr + nv_offset,
-		       nv_size);
-		rv = nv_size;
-	}
+	ret = chromeos_write_nvram_block(page, nv_lba);
+	if (!ret)
+		ret = nv_size;
 
-unwind:
+out:
 	__free_page(page);
-	return rv;
+	return ret;
 }
-
-static int chromeos_write(struct file *unused1, const char __user *data,
-			  size_t len, loff_t *unused2)
-{
-	u8 local_copy[MAX_NVRAM_BUFFER_SIZE];
-
-	if (!access_ok(VERIFY_READ, data, len))
-		return -EFAULT;
-
-	if (len != nv_size) {
-		pr_err(MODULE_NAME ": %d != %d, invalid block size\n",
-		       len, nv_size);
-		return -EINVAL;
-	}
-
-	if (copy_from_user(local_copy, data, len)) {
-		pr_err(MODULE_NAME ": copy failed!\n");
-		return -EFAULT;
-	}
-	return _chromeos_write(local_copy);
-}
-
-static const struct file_operations dbg_fops = {
-	.open		= debugfs_open,
-	.read		= seq_read,
-	.write		= chromeos_write,
-	.llseek		= seq_lseek,
-	.release	= single_release,
-};
-
 
 /*
  * Read the nvram buffer contents into the user provided space.
  *
- * retrun number of bytes copied, or -1 on any error.
+ * returns number of bytes copied, or negative error.
  */
 int chromeos_platform_read_nvram(u8 *nvram_buffer, int buf_size)
 {
-	if (!firmware_shared_data) {
-		pr_err(MODULE_NAME ": %s NVRAM not configured!\n", __func__);
-		return -1;
+	if (!nv_size) {
+		pr_err("%s nonvolatile context not configured!\n", __func__);
+		return -ENODEV;
 	}
 
 	if (buf_size < nv_size) {
-		pr_err(MODULE_NAME
-		       ": not enough room to read nvram (%d < %d)\n",
+		pr_err("not enough room to read nvram (%d < %d)\n",
 		       buf_size, nv_size);
-		return -1;
+		return -ENOSPC;
 	}
 
-	memcpy(nvram_buffer, firmware_shared_data->nvcxt_cache, nv_size);
-	return nv_size;
+	return _chromeos_read(nvram_buffer);
 }
 
 int chromeos_platform_write_nvram(u8 *nvram_buffer, int buf_size)
 {
-	if (!firmware_shared_data) {
-		pr_err(MODULE_NAME ": %sw NVRAM not configured!\n", __func__);
-		return -1;
+	if (!nv_size) {
+		pr_err("%s nonvolatile context not configured!\n", __func__);
+		return -ENODEV;
 	}
 
 	if (buf_size != nv_size) {
-		pr_err(MODULE_NAME ": wrong write buffer size (%d != %d)\n",
+		pr_err("wrong write buffer size (%d != %d)\n",
 		       buf_size, nv_size);
-		return -1;
+		return -ENOSPC;
 	}
 
 	return _chromeos_write(nvram_buffer);
 }
 
-static void __init chromeos_arm_shutdown(void)
-{
-	if (debugfs_entry) {
-		debugfs_remove(debugfs_entry);
-		debugfs_entry = NULL;
-	}
-
-	if (firmware_shared_data) {
-		iounmap(firmware_shared_data);
-		firmware_shared_data = NULL;
-	}
-}
-
 static int __init chromeos_arm_init(void)
 {
-	if (!phys_window_base) {
-		pr_err(MODULE_NAME ": memory window base undefined\n");
+	int proplen, err, size;
+	const int *prop;
+	struct device_node *fw_dn;
+
+	fw_dn = of_find_compatible_node(NULL, NULL, "chromeos-firmware");
+	if (!fw_dn)
 		return -ENODEV;
+
+	prop = of_get_property(fw_dn, "nonvolatile-context-offset", &proplen);
+	if (!prop || proplen != 4) {
+		pr_err("can't find nonvolatile memory offset\n");
+		err = -ENODEV;
+		goto err;
+	}
+	nv_offset = be32_to_cpup(prop);
+
+	prop = of_get_property(fw_dn, "boot-nonvolatile-cache", &proplen);
+	if (!prop) {
+		pr_err("can't find boot copy of nonvolatile cache\n");
+		err = -ENODEV;
+		goto err;
+	}
+	nv_size = proplen;
+
+	prop = of_get_property(fw_dn, "nonvolatile-context-size", &proplen);
+	if (!prop || proplen != 4) {
+		pr_err("can't find size of nonvolatile memory\n");
+		err = -ENODEV;
+		goto err;
+	}
+	size = be32_to_cpup(prop);
+
+	if (size != nv_size) {
+		pr_err("nvram size and cache mismatch\n");
+		err = -EINVAL;
+		goto err;
 	}
 
-	pr_info(MODULE_NAME ": memory window base at %llx\n", phys_window_base);
+	if ((nv_offset + nv_size > SECTOR_SIZE) ||
+	    (nv_size > MAX_NVRAM_BUFFER_SIZE)) {
+		/* nvram block won't fit into a sector */
+		pr_err("bad nvram location: %d:%d!\n", nv_offset, nv_size);
+		err = -EINVAL;
+		goto err;
+	}
 
-	/*
-	 * TODO(vbendeb): FIXME: we are getting a pointer to some memory which
-	 * is beyond the kernel controlled memory range. Strictly speaking
-	 * this pointer should not be just dereferenced, ioread()/iowrite()
-	 * should be used instead to comply with some code analysis tools and
-	 * run time checks (when enabled). A possible solution would be to
-	 * copy data from the window into a kernel allocated buffer.
-	 *
-	 * This will have to be addressed when the FDT rework comes along.
+	prop = of_get_property(fw_dn, "nonvolatile-context-lba", &proplen);
+	if (!prop) {
+		pr_err("can't find nvcontext lba\n");
+		err = -ENODEV;
+		goto err;
+	}
+	switch (proplen) {
+	case 4:
+		nv_lba = be32_to_cpup(prop);
+		break;
+	case 8:
+		nv_lba = be64_to_cpup((const __be64 *)prop);
+		break;
+	default:
+		pr_err("invalid nvcontext lba\n");
+		err = -EINVAL;
+		goto err;
+	}
+
+	/* XXXOJN FIXME: There should be a search for the right block device to
+	 * use for volatile storage here, not just assume mmcblk0. This should
+	 * include comparing the cached context passed in through the property.
 	 */
-	firmware_shared_data = ioremap(phys_window_base,
-				       FIRMWARE_SHARED_PHYSICAL_SIZE);
-	if (!firmware_shared_data) {
-		pr_err(MODULE_NAME ": failed to map address %llx\n",
-		       phys_window_base);
-		return -EINVAL;
-	}
 
-	nv_offset = firmware_shared_data->vbnv[0];
-	nv_size = firmware_shared_data->vbnv[1];
+	pr_info("chromeos system detected\n");
 
-	if (verify_shared_memory()) {
-		chromeos_arm_shutdown();
-		return -EINVAL;
-	}
+	err = 0;
+err:
+	of_node_put(fw_dn);
 
-
-	debugfs_entry = debugfs_create_file(MODULE_NAME, S_IRUGO | S_IWUSR,
-					    NULL, NULL, &dbg_fops);
-	if (!debugfs_entry) {
-		pr_err(MODULE_NAME ": failed to create a debugfs file!\n");
-		chromeos_arm_shutdown();
-		return -EINVAL;
-	}
-	pr_debug(MODULE_NAME ": firmware cookie lba = %llx\n",
-		 firmware_shared_data->nvcxt_lba);
-	return 0;
+	return err;
 }
 subsys_initcall(chromeos_arm_init);
-
-static int __init get_mem_base(char *p)
-{
-	char *endptr;   /* local pointer to end of parsed string */
-	u64 base = simple_strtoull(p, &endptr, 0);
-
-	/* rudimentary sanity check */
-	if (base & ((1 << 20) - 1)) {
-		pr_err(MODULE_NAME ": unaligned window base 0x%llx\n", base);
-		return -1;
-	}
-
-	phys_window_base = base;
-	return 0;
-}
-
-early_param("cros_shared_mem", get_mem_base);
