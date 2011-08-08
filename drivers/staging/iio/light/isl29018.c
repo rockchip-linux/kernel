59453693702c2d2b44b99314fd3952500a5ef753
Grant Grundler <grundler@chromium.org>
2011-08-08 09:45:08 -0700

    CHROMIUM:staging:iio:light: fix ISL29018 sensor operation after a brownout
    After a voltage brownout, sensor will now operate correctly.
    
    Page 10 of ISL29018 data sheet and the Intersil Application Note 1534
    describe the required initialization sequence:
    1. Write 0x00 to register 0x08 (TEST)
    2. Write 0x00 to register 0x00 (CMD1)
    3. msleep(1)
    4. program remaining registers as before
    
    BUG=chrome-os-partner:5351,chrome-os-partner:4752
    TEST=manual (boots and light sensor responds after 5 seconds)
    
    Change-Id: I09738e3680cacfa9f71e2173cdd02aae82e26dd5
    Signed-off-by: Grant Grundler <grundler@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/5474
    Reviewed-by: Bryan Freed <bfreed@chromium.org>
    Tested-by: Bryan Freed <bfreed@chromium.org>

--- drivers/staging/iio/light/isl29018.c
+++ drivers/staging/iio/light/isl29018.c
@@ -58,6 +58,10 @@
 #define ISL29018_TEST_SHIFT		0
 #define ISL29018_TEST_MASK		(0xFF << ISL29018_TEST_SHIFT)
 
+#define ISL29018_REG_TEST		0x08
+#define ISL29018_TEST_SHIFT		0
+#define ISL29018_TEST_MASK		(0xFF << ISL29018_TEST_SHIFT)
+
 struct isl29018_chip {
 	struct device		*dev;
 	struct regmap		*regmap;
