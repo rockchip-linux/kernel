572dfe5fd4925fc97aa469b4fe0cacd1791c0a77
Sameer Nanda <snanda@chromium.org>
2012-05-08 23:07:31 +0000

    CHROMIUM: scheduler: panic if schedulin while atomic
    When the schduling while atomic condition is hit, currently only a
    printk and dump_stack happens but the kernel continues to run.  We've
    run into issues where we think this causes a hang during shutdown
    eventually.
    
    Now doing a kernel panic instead by calling BUG().  This should prevent
    hangs at shutdown from happening as well as providing us stack traces
    that might help us root cause the real issue.
    
    Signed-off-by: Sameer Nanda <snanda@chromium.org>
    
    BUG=chromium-os:29599
    TEST=We don't have a good way of repro'ing the hang on shutdown so the
    testing is limited to kernel building fine.
    
    Change-Id: I52084abf64a133a00f20e2eeaeb921820755e252
    Reviewed-on: https://gerrit.chromium.org/gerrit/21534
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    Commit-Ready: Sameer Nanda <snanda@chromium.org>
    Tested-by: Sameer Nanda <snanda@chromium.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2554,8 +2554,7 @@ static noinline void __schedule_bug(struct task_struct *prev)
 	print_modules();
 	if (irqs_disabled())
 		print_irqtrace_events(prev);
-	dump_stack();
-	add_taint(TAINT_WARN, LOCKDEP_STILL_OK);
+	BUG();
 }
 
 /*
