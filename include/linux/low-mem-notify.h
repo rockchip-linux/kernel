813d5b1199ecaffc9707f546ff8e04269d76d38b
Luigi Semenzato <semenzato@chromium.org>
2012-07-02 16:15:39 -0700

    CHROMIUM: low-mem: work with swap enabled
    Without this patch, the notifier triggers too early, when no or
    little swap space is used.
    
    We add a contribution from the swap to the "available memory" concept
    by introducing a "RAM vs. swap weight".  We can't just add them
    together because 1. using swap costs us RAM; and 2. freeing RAM by
    swapping its content out takes longer than using already free
    RAM.
    
    Experiment on an Alex with 2Gb of RAM show that with a weight of
    4 and a 50Mb threshold, the system does not thrash or OOM.  (The
    experiments are not a complete guarantee.)
    
    BUG=chromium-os:32322
    TEST=tested with a memory pressure autotest which I am not allowed to commit
    
    Signed-off-by: Luigi Semenzato <semenzato@chromium.org>
    
    Change-Id: I614aa3e1ab1186ccc715bd23a082e7f9379b181c
    Reviewed-on: https://gerrit.chromium.org/gerrit/26616
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Commit-Ready: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    
    [bleung: 3.10 rebase : fixup with atomic_long_read(&nr_swap_pages)]
    Signed-off-by: Benson Leung <bleung@chromium.org>

--- include/linux/low-mem-notify.h
+++ include/linux/low-mem-notify.h
@@ -1,8 +1,9 @@
 #ifndef _LINUX_LOW_MEM_NOTIFY_H
 #define _LINUX_LOW_MEM_NOTIFY_H
 
-#include <linux/stddef.h>
 #include <linux/mm.h>
+#include <linux/stddef.h>
+#include <linux/swap.h>
 
 extern unsigned low_mem_margin_percent;
 extern unsigned long low_mem_minfree;
@@ -13,17 +14,12 @@ extern unsigned long low_mem_lowest_seen_anon_mem;
 extern const unsigned long low_mem_anon_mem_delta;
 
 /*
- * Return TRUE if we are in a low memory state.
+ * Compute "available" memory, that is either free memory or memory that can be
+ * reclaimed quickly.
  */
-static inline bool _is_low_mem_situation(void)
+static inline unsigned long get_available_mem(int lru_base)
 {
-	const int lru_base = NR_LRU_BASE - LRU_BASE;
-	static bool was_low_mem;	/* style guide prohibits "= false" */
-	/*
-	 * We declare a low-memory condition when free memory plus easily
-	 * reclaimable memory is low.
-	 *
-	 * free_mem is completely unallocated; clean file-backed memory
+	/* free_mem is completely unallocated; clean file-backed memory
 	 * (file_mem - dirty_mem) is easy to reclaim, except for the last
 	 * min_filelist_kbytes.
 	 */
@@ -35,6 +31,24 @@ static inline bool _is_low_mem_situation(void)
 	unsigned long min_file_mem = min_filelist_kbytes >> (PAGE_SHIFT - 10);
 	unsigned long available_file_mem = file_mem - dirty_mem - min_file_mem;
 	unsigned long available_mem = free_mem + available_file_mem;
+	return available_mem;
+}
+
+/*
+ * Return TRUE if we are in a low memory state.
+ */
+static inline bool _is_low_mem_situation(void)
+{
+	const int lru_base = NR_LRU_BASE - LRU_BASE;
+	static bool was_low_mem;	/* = false, as per style guide */
+	/* We declare a low-memory condition when a combination of RAM and swap
+	 * space is low.  The contribution of swap is reduced by a factor of
+	 * ram_vs_swap_weight.
+	 */
+	const int ram_vs_swap_weight = 4;
+	long _nr_swap_pages = atomic_long_read(&nr_swap_pages);
+	unsigned long available_mem = get_available_mem(lru_base) +
+		_nr_swap_pages / ram_vs_swap_weight;
 	bool is_low_mem = available_mem < low_mem_minfree;
 
 	if (unlikely(is_low_mem && !was_low_mem)) {
@@ -42,9 +56,11 @@ static inline bool _is_low_mem_situation(void)
 			global_page_state(lru_base + LRU_ACTIVE_ANON) +
 			global_page_state(lru_base + LRU_INACTIVE_ANON);
 		if (anon_mem < low_mem_lowest_seen_anon_mem) {
-			printk(KERN_INFO "entering low_mem (avail = %lu kB) "
+			printk(KERN_INFO "entering low_mem "
+			       "(avail RAM = %lu kB, avail swap %lu kB) "
 			       "with lowest seen anon mem: %lu kB\n",
 			       available_mem * PAGE_SIZE / 1024,
+			       _nr_swap_pages * PAGE_SIZE / 1024,
 			       anon_mem * PAGE_SIZE / 1024);
 			low_mem_lowest_seen_anon_mem = anon_mem -
 				low_mem_anon_mem_delta;
