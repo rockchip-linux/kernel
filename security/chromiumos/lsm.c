caa202c1875d4d7b8052d45782de8c96f3ce90ed
Kees Cook <keescook@chromium.org>
2012-09-04 15:28:15 -0700

    CHROMIUM: LSM: add module loading restrictions
    Only allow modules to be loaded from the same initial file system.
    
    At run-time, if root device is writable (dm-verity disabled), the restriction
    can be disabled via the sysctl "/proc/sys/kernel/chromiumos/module_locking".
    
    BUG=chromium-os:34134
    TEST=parrot build & boot, manual testing
    
    Change-Id: I2ee7b15fb70b342ba8ffbdb74a0377215187924a
    [sonnyrao: 3.8: squashed with LSM: allow old style loads when locking diabled]
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/34306
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    
    [bleung : 3.10 rebase. removed use of f_vfsmnt for f_path.mnt]
    Signed-off-by: Benson Leung <bleung@chromium.org>

--- security/chromiumos/lsm.c
+++ security/chromiumos/lsm.c
@@ -3,8 +3,9 @@
  *
  * Copyright 2011 Google Inc. All Rights Reserved
  *
- * Author:
+ * Authors:
  *      Stephan Uphoff  <ups@google.com>
+ *      Kees Cook       <keescook@chromium.org>
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -16,9 +17,18 @@
  * GNU General Public License for more details.
  */
 
+#define pr_fmt(fmt) "Chromium OS LSM: " fmt
+
 #include <linux/module.h>
 #include <linux/security.h>
 #include <linux/sched.h>	/* current and other task related stuff */
+#include <linux/fs.h>
+#include <linux/fs_struct.h>
+#include <linux/mount.h>
+#include <linux/path.h>
+#include <linux/root_dev.h>
+
+#include "utils.h"
 
 static int chromiumos_security_sb_mount(const char *dev_name, struct path *path,
 					const char *type, unsigned long flags,
@@ -27,18 +37,163 @@ static int chromiumos_security_sb_mount(const char *dev_name, struct path *path,
 	int error = current->total_link_count ? -ELOOP : 0;
 
 	if (error) {
-		char name[sizeof(current->comm)];
-		printk(KERN_NOTICE "Chromium OS LSM: Mount path with symlinks"
-			" prohibited - Task %s (pid = %d)\n",
-			get_task_comm(name, current), task_pid_nr(current));
+		char *cmdline;
+
+		cmdline = printable_cmdline(current);
+		pr_notice("Mount path with symlinks prohibited - "
+			"pid=%d cmdline=%s\n",
+			task_pid_nr(current), cmdline);
+		kfree(cmdline);
 	}
 
 	return error;
 }
 
+static void report_load_module(struct path *path, char *operation)
+{
+	char *alloced = NULL, *cmdline;
+	char *pathname; /* Pointer to either static string or "alloced". */
+
+	if (!path)
+		pathname = "<unknown>";
+	else {
+		/* We will allow 11 spaces for ' (deleted)' to be appended */
+		alloced = pathname = kmalloc(PATH_MAX+11, GFP_KERNEL);
+		if (!pathname)
+			pathname = "<no_memory>";
+		else {
+			pathname = d_path(path, pathname, PATH_MAX+11);
+			if (IS_ERR(pathname))
+				pathname = "<too_long>";
+			else {
+				pathname = printable(pathname);
+				kfree(alloced);
+				alloced = pathname;
+			}
+		}
+	}
+
+	cmdline = printable_cmdline(current);
+
+	pr_notice("init_module %s module=%s pid=%d cmdline=%s\n",
+		  operation, pathname, task_pid_nr(current), cmdline);
+
+	kfree(cmdline);
+	kfree(alloced);
+}
+
+static int module_locking = 1;
+static struct dentry *locked_root;
+
+#ifdef CONFIG_SYSCTL
+static int zero;
+static int one = 1;
+
+static struct ctl_path chromiumos_sysctl_path[] = {
+	{ .procname = "kernel", },
+	{ .procname = "chromiumos", },
+	{ }
+};
+
+static struct ctl_table chromiumos_sysctl_table[] = {
+	{
+		.procname       = "module_locking",
+		.data           = &module_locking,
+		.maxlen         = sizeof(int),
+		.mode           = 0644,
+		.proc_handler   = proc_dointvec_minmax,
+		.extra1         = &zero,
+		.extra2         = &one,
+	},
+	{ }
+};
+
+/* Check if the root device is read-only (e.g. dm-verity is enabled).
+ * This must be called after early kernel init, since then the rootdev
+ * is available.
+ */
+static bool rootdev_readonly(void)
+{
+	bool rc;
+	struct block_device *bdev;
+	const fmode_t mode = FMODE_WRITE;
+
+	bdev = blkdev_get_by_dev(ROOT_DEV, mode, NULL);
+	if (IS_ERR(bdev)) {
+		/* In this weird case, assume it is read-only. */
+		pr_info("dev(%u,%u): FMODE_WRITE disallowed?!\n",
+			MAJOR(ROOT_DEV), MINOR(ROOT_DEV));
+		return true;
+	}
+
+	rc = bdev_read_only(bdev);
+	blkdev_put(bdev, mode);
+
+	pr_info("dev(%u,%u): %s\n", MAJOR(ROOT_DEV), MINOR(ROOT_DEV),
+		rc ? "read-only" : "writable");
+
+	return rc;
+}
+
+static void check_locking_enforcement(void)
+{
+	/* If module locking is not being enforced, allow sysctl to change
+	 * modes for testing.
+	 */
+	if (!rootdev_readonly()) {
+		if (!register_sysctl_paths(chromiumos_sysctl_path,
+					   chromiumos_sysctl_table))
+			pr_notice("sysctl registration failed!\n");
+		else
+			pr_info("module locking can be disabled.\n");
+	} else
+		pr_info("module locking engaged.\n");
+}
+#else
+static void check_locking_enforcement(void) { }
+#endif
+
+
+static int chromiumos_security_load_module(struct file *file)
+{
+	struct dentry *module_root;
+
+	if (!file) {
+		if (!module_locking) {
+			report_load_module(NULL, "old-api-locking-ignored");
+			return 0;
+		}
+
+		report_load_module(NULL, "old-api-denied");
+		return -EPERM;
+	}
+
+	module_root = file->f_path.mnt->mnt_root;
+
+	/* First loaded module defines the root for all others. */
+	if (!locked_root) {
+		locked_root = dget(module_root);
+		report_load_module(&file->f_path, "locked");
+		check_locking_enforcement();
+	}
+
+	if (module_root != locked_root) {
+		if (unlikely(!module_locking)) {
+			report_load_module(&file->f_path, "locking-ignored");
+			return 0;
+		}
+
+		report_load_module(&file->f_path, "denied");
+		return -EPERM;
+	}
+
+	return 0;
+}
+
 static struct security_operations chromiumos_security_ops = {
 	.name	= "chromiumos",
 	.sb_mount = chromiumos_security_sb_mount,
+	.kernel_module_from_file = chromiumos_security_load_module,
 };
 
 
@@ -49,8 +204,11 @@ static int __init chromiumos_security_init(void)
 	error = register_security(&chromiumos_security_ops);
 
 	if (error)
-		panic("Could not register chromiumos security module");
+		panic("Could not register Chromium OS security module");
 
 	return error;
 }
 security_initcall(chromiumos_security_init);
+
+module_param(module_locking, int, S_IRUSR);
+MODULE_PARM_DESC(module_locking, "Module loading restrictions (default: true)");
