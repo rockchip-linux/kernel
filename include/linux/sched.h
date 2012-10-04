d932d9ae6c6934a93f03d38d086d43e08af660f6
Thomas Gleixner <tglx@linutronix.de>
2012-10-04 14:20:47 +0100

    softirq: Split softirq locks
    The 3.x RT series removed the split softirq implementation in favour
    of pushing softirq processing into the context of the thread which
    raised it. Though this prevents us from handling the various softirqs
    at different priorities. Now instead of reintroducing the split
    softirq threads we split the locks which serialize the softirq
    processing.
    
    If a softirq is raised in context of a thread, then the softirq is
    noted on a per thread field, if the thread is in a bh disabled
    region. If the softirq is raised from hard interrupt context, then the
    bit is set in the flag field of ksoftirqd and ksoftirqd is invoked.
    When a thread leaves a bh disabled region, then it tries to execute
    the softirqs which have been raised in its own context. It acquires
    the per softirq / per cpu lock for the softirq and then checks,
    whether the softirq is still pending in the per cpu
    local_softirq_pending() field. If yes, it runs the softirq. If no,
    then some other task executed it already. This allows for zero config
    softirq elevation in the context of user space tasks or interrupt
    threads.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1832,6 +1832,8 @@ struct task_struct {
 #endif
 #ifdef CONFIG_PREEMPT_RT_BASE
 	struct rcu_head put_rcu;
+	int softirq_nestcnt;
+	unsigned int softirqs_raised;
 #endif
 #ifdef CONFIG_DEBUG_ATOMIC_SLEEP
 	unsigned long	task_state_change;
@@ -2096,6 +2098,7 @@ extern void thread_group_cputime_adjusted(struct task_struct *p, cputime_t *ut, 
 /*
  * Per process flags
  */
+#define PF_IN_SOFTIRQ	0x00000001	/* Task is serving softirq */
 #define PF_EXITING	0x00000004	/* getting shut down */
 #define PF_EXITPIDONE	0x00000008	/* pi exit done on shut down */
 #define PF_VCPU		0x00000010	/* I'm a virtual CPU */
