5c2b7b948ecd106d8e850ae9b9f4bae9162bc7e5
Paul Taysom <taysom@chromium.org>
2013-03-24 19:58:17 -0700

    CHROMIUM: md: dm-verity: call to verity_error missing
    The code path going through verity_work, was not calling
    verity_error. This meant invalid block errors were not
    being passed to the verity-chromeos error handler.
    
    BUG=chromium:222545
    TEST=manually corrupt root file system, reboot
    
    Change-Id: I0095e7fc8034f324ee9fc4540506bad0f3aee99a
    Signed-off-by: Paul Taysom <taysom@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/46376
    Reviewed-by: Mandeep Singh Baines <msb@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/51365
    Commit-Queue: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Sonny Rao <sonnyrao@chromium.org>

--- drivers/md/dm-verity.c
+++ drivers/md/dm-verity.c
@@ -534,6 +534,8 @@ static void verity_finish_io(struct dm_verity_io *io, int error)
 	struct dm_verity *v = io->v;
 	struct bio *bio = dm_bio_from_per_bio_data(io, v->ti->per_bio_data_size);
 
+	if (error)
+		verity_error(v, io, error);
 	bio->bi_end_io = io->orig_bi_end_io;
 	bio->bi_private = io->orig_bi_private;
 
@@ -550,12 +552,6 @@ static void verity_work(struct work_struct *w)
 static void verity_end_io(struct bio *bio, int error)
 {
 	struct dm_verity_io *io = bio->bi_private;
-
-	if (error) {
-		verity_error(io->v, io, error);
-		verity_finish_io(io, error);
-		return;
-	}
 
 	INIT_WORK(&io->work, verity_work);
 	queue_work(io->v->verify_wq, &io->work);
