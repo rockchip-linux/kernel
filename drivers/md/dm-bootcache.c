bb0d66fd15b2aaf5d0cc237b45ccdef2ee86e3fa
Sonny Rao <sonnyrao@chromium.org>
2013-03-26 14:28:36 -0700

    CHROMIUM: md: bootcache compile fixes for 3.8
    Remove references to bootcache_bio_destructor, fix function signatures
    for status and map dm functions.
    
    Change-Id: I54815863acb1a8932029f7a3fff4e9774751e193
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>

--- drivers/md/dm-bootcache.c
+++ drivers/md/dm-bootcache.c
@@ -148,13 +148,6 @@ static inline u64 pages_to_sectors(u64 pages)
 	return pages << (PAGE_SHIFT - SECTOR_SHIFT);
 }
 
-static void bootcache_bio_destructor(struct bio *bio)
-{
-	struct bootcache *cache = bio->bi_private;
-
-	bio_free(bio, cache->bio_set);
-}
-
 static inline struct bootcache_page **bootcache_hash(
 					struct bootcache_sector_map *map,
 					u64 sector)
@@ -583,7 +576,6 @@ static int bootcache_read_sectors(struct bootcache *cache)
 		return -ENOMEM;
 	}
 	bio->bi_private = &waiter;
-	bio->bi_destructor = bootcache_bio_destructor;
 	p = cache->sectors.pages;
 	for (i = 0; i < chunks_to_read; i++) {
 		bio->bi_idx = 0;
@@ -666,7 +658,6 @@ static int bootcache_dev_read(struct bootcache *cache, void *data,
 	for (i = 0; i < num_pages; i++, bvec++)
 		bvec->bv_page = alloc_page(GFP_KERNEL);
 	bio->bi_private = &waiter;
-	bio->bi_destructor = bootcache_bio_destructor;
 	pages_read = 0;
 	while (len) {
 		if (pages_to_read < max_io)
@@ -1163,7 +1154,7 @@ bad_cache:
 }
 
 static int bootcache_status(struct dm_target *ti, status_type_t type,
-				char *result, uint maxlen)
+			    unsigned status_flags, char *result, uint maxlen)
 {
 	struct bootcache *cache = (struct bootcache *) ti->private;
 	uint sz = 0;
@@ -1210,8 +1201,7 @@ static void bootcache_dtr(struct dm_target *ti)
 	kfree(cache);
 }
 
-static int bootcache_map(struct dm_target *ti, struct bio *bio,
-			union map_info *map_context)
+static int bootcache_map(struct dm_target *ti, struct bio *bio)
 {
 	bootcache_read(ti->private, bio);
 	return DM_MAPIO_SUBMITTED;
