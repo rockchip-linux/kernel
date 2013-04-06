2bd68450b492263002aa6a2f8bd61f9290fc7fb1
Sonny Rao <sonnyrao@chromium.org>
2013-04-05 17:54:16 -0700

    CHROMIUM: security: enable VFS link restrictions by default
    This enables the protected symlink and hardlink sysctls by default.
    
    Change-Id: I7725cb4e4342022e469b22a7156ba6754b571a96
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>

--- fs/namei.c
+++ fs/namei.c
@@ -700,8 +700,8 @@ static inline void put_link(struct nameidata *nd, struct path *link, void *cooki
 	path_put(link);
 }
 
-int sysctl_protected_symlinks __read_mostly = 0;
-int sysctl_protected_hardlinks __read_mostly = 0;
+int sysctl_protected_symlinks __read_mostly = 1;
+int sysctl_protected_hardlinks __read_mostly = 1;
 
 /**
  * may_follow_link - Check symlink following for unsafe situations
