c49679f9fc9edc3bf107bc4a84e415eaf8786fc7
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2013-06-22 03:21:18 +0300

    zram: remove zram_sysfs file (v2)
    (cherry pick from commit 9b3bb7abcdf2df0f1b2657e6cbc9d06bc2b3b36f)
    
    Move zram sysfs code to zram drv and remove zram_sysfs.c
    file. This gives ability to make static a number of previously
    exported zram functions, used from zram sysfs, e.g. internal zram
    zram_meta_alloc/free(). We also can drop zram_drv wrapper
    functions, used from zram sysfs:
    e.g. zram_reset_device()/__zram_reset_device() pair.
    
    v2: as suggested by Greg K-H, move MODULE description to the
    bottom of the file.
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Bug: 24810447
    Change-Id: Ic0038af190db194f7891cb27502a9b6d0d9e4042

--- drivers/staging/zram/zram_drv.h
+++ drivers/staging/zram/zram_drv.h
@@ -114,13 +114,5 @@ struct zram {
 
 extern struct zram *zram_devices;
 unsigned int zram_get_num_devices(void);
-#ifdef CONFIG_SYSFS
-extern struct attribute_group zram_disk_attr_group;
-#endif
-
-extern void zram_reset_device(struct zram *zram);
-extern struct zram_meta *zram_meta_alloc(u64 disksize);
-extern void zram_meta_free(struct zram_meta *meta);
-extern void zram_init_device(struct zram *zram, struct zram_meta *meta);
 
 #endif
