6399ae368231d2e11e092c7a2b7c47455eae881b
Simon Glass <sjg@chromium.org>
2013-06-24 16:41:17 -0700

    cros_ec: Make sure mutex is unlocked in SPI EAGAIN case
    The code which handles the EC not being ready does not deal with the mutex
    properly. Fix it.
    
    BUG=chrome-os-partner:20378
    TEST=manual
    Build and boot on pit
    Verified by code inspection. So far I cannot repeat this problem after
    50 reboots.
    
    Change-Id: Ibcc7b6ac1e18ed5d423c5e48af215097532b30dc
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/59854
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>

--- drivers/mfd/cros_ec_spi.c
+++ drivers/mfd/cros_ec_spi.c
@@ -296,7 +296,8 @@ static int cros_ec_cmd_xfer_spi(struct cros_ec_device *ec_dev,
 		if (ptr[0] == EC_RES_IN_PROGRESS) {
 			dev_dbg(ec_dev->dev, "command 0x%02x in progress\n",
 				ec_msg->cmd);
-			return -EAGAIN;
+			ret = -EAGAIN;
+			goto exit;
 		}
 		dev_warn(ec_dev->dev, "command 0x%02x returned an error %d\n",
 			 ec_msg->cmd, ptr[0]);
