064fe07e7cb56c152c95ff6c1733c64c355fb1d6
Minchan Kim <minchan@kernel.org>
2013-08-12 15:13:55 +0900

    zram: fix invalid memory access
    (cherry pick from commit 2b86ab9cc29fcd435cde9378c3b9ffe8b5c76128)
    
    [1] tried to fix invalid memory access on zram->disk but it didn't
    fix properly because get_disk failed during module exit path.
    
    Actually, we don't need to reset zram->disk's capacity to zero
    in module exit path so that this patch introduces new argument
    "reset_capacity" on zram_reset_divice and it only reset it when
    reset_store is called.
    
    [1] 6030ea9b,  zram: avoid invalid memory access in zram_exit()
    
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: Jiang Liu <jiang.liu@huawei.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Bug: 24810447
    Change-Id: I87e35bc5af6f82a75a9cdc620a142f86e80f2884

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -521,7 +521,7 @@ static int zram_bvec_rw(struct zram *zram, struct bio_vec *bvec, u32 index,
 	return ret;
 }
 
-static void zram_reset_device(struct zram *zram)
+static void zram_reset_device(struct zram *zram, bool reset_capacity)
 {
 	size_t index;
 	struct zram_meta *meta;
@@ -547,7 +547,8 @@ static void zram_reset_device(struct zram *zram)
 	memset(&zram->stats, 0, sizeof(zram->stats));
 
 	zram->disksize = 0;
-	set_capacity(zram->disk, 0);
+	if (reset_capacity)
+		set_capacity(zram->disk, 0);
 	up_write(&zram->init_lock);
 }
 
@@ -631,7 +632,7 @@ static ssize_t reset_store(struct device *dev,
 	if (bdev)
 		fsync_bdev(bdev);
 
-	zram_reset_device(zram);
+	zram_reset_device(zram, true);
 	return len;
 }
 
@@ -899,7 +900,11 @@ static void __exit zram_exit(void)
 		zram = &zram_devices[i];
 
 		destroy_device(zram);
-		zram_reset_device(zram);
+		/*
+		 * Shouldn't access zram->disk after destroy_device
+		 * because destroy_device already released zram->disk.
+		 */
+		zram_reset_device(zram, false);
 	}
 
 	unregister_blkdev(zram_major, "zram");
