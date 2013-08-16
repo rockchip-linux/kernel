0ffc05795d0b00460bc843f01dae7ddaac9e5274
Kees Cook <keescook@chromium.org>
2013-08-16 00:11:32 -0700

    UPSTREAM: HID: multitouch: validate feature report details
    When working on report indexes, always validate that they are in bounds.
    Without this, a HID device could report a malicious feature report that
    could trick the driver into a heap overflow:
    
    [  634.885003] usb 1-1: New USB device found, idVendor=0596, idProduct=0500
    ...
    [  676.469629] BUG kmalloc-192 (Tainted: G        W   ): Redzone overwritten
    
    CVE-2013-2897
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Cc: stable@kernel.org
    
    BUG=chromium:280729
    TEST=link build, exploit fails
    
    Change-Id: Id61d1fafd4f1ef0e87ec95f19c3325402fd8a518
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/167520
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -320,12 +320,21 @@ static void mt_feature_mapping(struct hid_device *hdev,
 			dev_err(&hdev->dev, "HID_DG_INPUTMODE out of range\n");
 			break;
 		}
+		/* Ignore if value index is out of bounds. */
+		if (td->inputmode_index < 0 ||
+		    td->inputmode_index >= field->report_count) {
+			dev_err(&hdev->dev, "HID_DG_INPUTMODE out of range\n");
+			td->inputmode = -1;
+		}
 
 		td->inputmode = field->report->id;
 		td->inputmode_index = usage->usage_index;
 
 		break;
 	case HID_DG_CONTACTMAX:
+		/* Ignore if value count is out of bounds. */
+		if (field->report_count < 1)
+			break;
 		td->maxcontact_report_id = field->report->id;
 		td->maxcontacts = field->value[0];
 		if (!td->maxcontacts &&
