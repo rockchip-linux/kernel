a515d5a0e01803cc2581a706580aff80809e3f68
Andrew Bresticker <abrestic@chromium.org>
2013-12-12 14:52:55 -0800

    CHROMIUM: Input: atmel_mxt_ts: fix out-of-bounds array accesses
    The current_id map is fixed to a size of MXT_MAX_FINGER (10), but
    the touch controller may generate reports for a larger number of
    IDs.  On Nyan, for example, the controller claims it can report
    up to 16 touch IDs.  During initialization of the touchscreen,
    events were generated for all 16 touch IDs causing out-of-bounds
    writes to the current_id map.  Instead of fixing the size to 10,
    dynamically allocate the current_id map based on the number of
    touch IDs the controller claims to support.
    
    BUG=none
    TEST=Build and boot on Nyan.  evtest output looks reasonable.
    Suspend/resume is no longer broken.
    
    Change-Id: Iabf5f91839ed1523374209beac7daa209bbf060d
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/179758
    Reviewed-by: Yufeng Shen <miletus@chromium.org>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -281,8 +281,6 @@
 /* Touchscreen absolute values */
 #define MXT_MAX_AREA		0xff
 
-#define MXT_MAX_FINGER		10
-
 /* Fallback T7 values to restore functionality in the event of i2c problems */
 #define FALLBACK_MXT_POWER_IDLEACQINT	0xff
 #define FALLBACK_MXT_POWER_ACTVACQINT	0xff
@@ -420,7 +418,7 @@ struct mxt_data {
 	char *config_file;
 
 	/* map for the tracking id currently being used */
-	bool current_id[MXT_MAX_FINGER];
+	bool *current_id;
 
 	bool lid_handler_registered;
 	struct input_handler lid_handler;
@@ -555,7 +553,7 @@ static void mxt_release_all_fingers(struct mxt_data *data)
 	int max_area_channels = min(255U, data->max_area_channels);
 	int max_touch_major = get_touch_major_pixels(data, max_area_channels);
 	bool need_update = false;
-	for (id = 0; id < MXT_MAX_FINGER; id++) {
+	for (id = 0; id < data->num_touchids; id++) {
 		if (data->current_id[id]) {
 			dev_warn(dev, "Move touch %d to (0,0)\n", id);
 			input_mt_slot(input_dev, id);
@@ -572,7 +570,7 @@ static void mxt_release_all_fingers(struct mxt_data *data)
 	if (need_update)
 		input_sync(data->input_dev);
 
-	for (id = 0; id < MXT_MAX_FINGER; id++) {
+	for (id = 0; id < data->num_touchids; id++) {
 		if (data->current_id[id]) {
 			dev_warn(dev, "Release touch contact %d\n", id);
 			input_mt_slot(input_dev, id);
@@ -1468,12 +1466,18 @@ static int mxt_get_object_table(struct mxt_data *data)
 		}
 	}
 
+	data->current_id = kzalloc(sizeof(*data->current_id) *
+				   data->num_touchids, GFP_KERNEL);
+	if (!data->current_id)
+		return -ENOMEM;
+
 	return 0;
 }
 
 static void mxt_free_object_table(struct mxt_data *data)
 {
 	kfree(data->object_table);
+	kfree(data->current_id);
 	data->object_table = NULL;
 	data->T6_reportid = 0;
 	data->T9_reportid_min = 0;
@@ -1481,7 +1485,7 @@ static void mxt_free_object_table(struct mxt_data *data)
 	data->T19_reportid = 0;
 	data->T100_reportid_min = 0;
 	data->T100_reportid_max = 0;
-
+	data->num_touchids = 0;
 }
 
 static int mxt_initialize(struct mxt_data *data)
