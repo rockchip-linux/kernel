7e655dd9763becebd51c9a8284a933298e08097d
Marek Szyprowski <m.szyprowski@samsung.com>
2014-02-28 14:42:54 +0100

    UPSTREAM: arm: add support for reserved memory defined by device tree
    Enable reserved memory initialization from device tree.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Grant Likely <grant.likely@linaro.org>
    (cherry picked from commit bcedb5f9bd74662968fc1b4cb22f24abb4b7723d)
    
    Signed-off-by: Liang Chen <cl@rock-chips.com>
    Change-Id: Ieb41f2ec35b9145bfcad6f53b97720519f0ad4ef

--- arch/arm/mm/init.c
+++ arch/arm/mm/init.c
@@ -385,6 +385,8 @@ void __init arm_memblock_init(struct meminfo *mi, struct machine_desc *mdesc)
 	if (mdesc->reserve)
 		mdesc->reserve();
 
+	early_init_fdt_scan_reserved_mem();
+
 	/*
 	 * reserve memory for DMA contigouos allocations,
 	 * must come from DMA area inside low memory
