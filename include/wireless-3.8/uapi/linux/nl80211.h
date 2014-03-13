2aa70533a773eb16a3a98ce5900dbf55834284c6
Paul Stewart <pstew@chromium.org>
2014-03-13 11:51:48 -0700

    UPSTREAM: nl80211: Update nl80211_attrs to 3.10
    Update the nl80211_attrs in wireless38 to contain values from 3.10.
    
    Signed-off-by: Paul Stewart <pstew@chromium.org>
    
    BUG=chromium:352260
    TEST=Rebuild chromeos_kernel-3_10 with wireless38
    
    Change-Id: Ie9c0c3961e5754117de8cc0e4df8bd04b21e6657
    Reviewed-on: https://chromium-review.googlesource.com/189855
    Reviewed-by: Paul Stewart <pstew@chromium.org>
    Commit-Queue: Paul Stewart <pstew@chromium.org>
    Tested-by: Paul Stewart <pstew@chromium.org>

--- include/wireless-3.8/uapi/linux/nl80211.h
+++ include/wireless-3.8/uapi/linux/nl80211.h
@@ -1332,6 +1332,30 @@ enum nl80211_commands {
  * @NL80211_ATTR_EXT_CAPA_MASK: Extended capabilities that the kernel driver
  *	has set in the %NL80211_ATTR_EXT_CAPA value, for multibit fields.
  *
+ * @NL80211_ATTR_STA_CAPABILITY: Station capabilities (u16) are advertised to
+ *	the driver, e.g., to enable TDLS power save (PU-APSD).
+ *
+ * @NL80211_ATTR_STA_EXT_CAPABILITY: Station extended capabilities are
+ *	advertised to the driver, e.g., to enable TDLS off channel operations
+ *	and PU-APSD.
+ *
+ * @NL80211_ATTR_PROTOCOL_FEATURES: global nl80211 feature flags, see
+ *	&enum nl80211_protocol_features, the attribute is a u32.
+ *
+ * @NL80211_ATTR_SPLIT_WIPHY_DUMP: flag attribute, userspace supports
+ *	receiving the data for a single wiphy split across multiple
+ *	messages, given with wiphy dump message
+ *
+ * @NL80211_ATTR_MDID: Mobility Domain Identifier
+ *
+ * @NL80211_ATTR_IE_RIC: Resource Information Container Information
+ *	Element
+ *
+ * @NL80211_ATTR_CRIT_PROT_ID: critical protocol identifier requiring increased
+ *	reliability, see &enum nl80211_crit_proto_id (u16).
+ * @NL80211_ATTR_MAX_CRIT_PROT_DURATION: duration in milliseconds in which
+ *      the connection should have increased reliability (u16).
+ *
  * @NL80211_ATTR_MAX: highest attribute number currently defined
  * @__NL80211_ATTR_AFTER_LAST: internal use
  */
@@ -1614,6 +1638,21 @@ enum nl80211_attrs {
 
 	NL80211_ATTR_EXT_CAPA,
 	NL80211_ATTR_EXT_CAPA_MASK,
+
+	NL80211_ATTR_STA_CAPABILITY,
+	NL80211_ATTR_STA_EXT_CAPABILITY,
+
+	NL80211_ATTR_PROTOCOL_FEATURES,
+	NL80211_ATTR_SPLIT_WIPHY_DUMP,
+
+	NL80211_ATTR_DISABLE_VHT,
+	NL80211_ATTR_VHT_CAPABILITY_MASK,
+
+	NL80211_ATTR_MDID,
+	NL80211_ATTR_IE_RIC,
+
+	NL80211_ATTR_CRIT_PROT_ID,
+	NL80211_ATTR_MAX_CRIT_PROT_DURATION,
 
 	/* add attributes here, update the policy in nl80211.c */
 
