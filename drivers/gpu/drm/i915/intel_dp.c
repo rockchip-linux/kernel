60898abada3ddb9451359d9579d26ac37980e48f
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-03-21 23:22:35 +0100

    UPSTREAM: drm/i915: add locking to fixed panel edid probing
    With the recent addition of locking checks in
    
    commit 62ff94a5492175759546f8bc61383189d6b49122
    Author:     Daniel Vetter <daniel.vetter@ffwll.ch>
    AuthorDate: Thu Jan 23 22:18:47 2014 +0100
    
        drm/crtc-helper: remove LOCKING from kerneldoc
    
    drm_add_edid_modes started to WARN about the mode_config.mutex not
    being held in the lvds and dp initialization code.
    
    Now since this is init code locking is fairly redudant if it wouldn't
    be for the drm core registering sysfs files a bit early. And the
    locking WARNINGs nicely enforce that indeed all access to the mode
    lists are properly protected. And a full audit shows that only i915
    and gma500 touch the modes lists at init time.
    
    Hence I've opted to wrap up this entire mode detection sequence for
    fixed panels with the mode_config mutex for both lvds and edp outputs.
    
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit 060c877848b8a0a44d7ba32ff3a53982a6e907f7)
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>
    
    BUG=chrome-os-partner:34429
    TEST=No kernel warning from drm_mode_probed_add during i915 init.
    
    Change-Id: I4a729e6298e1017e714e739a1105b470040fa593
    Reviewed-on: https://chromium-review.googlesource.com/233920
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Tested-by: Matthew E Hiltner <matthew.e.hiltner@intel.com>
    Commit-Queue: Ullysses A Eoff <ullysses.a.eoff@intel.com>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -3738,6 +3738,7 @@ static bool intel_edp_init_connector(struct intel_dp *intel_dp,
 	/* We now know it's not a ghost, init power sequence regs. */
 	intel_dp_init_panel_power_sequencer_registers(dev, intel_dp, power_seq);
 
+	mutex_lock(&dev->mode_config.mutex);
 	edid = drm_get_edid(connector, &intel_dp->adapter);
 	if (edid) {
 		if (drm_add_edid_modes(connector, edid)) {
@@ -3768,6 +3769,7 @@ static bool intel_edp_init_connector(struct intel_dp *intel_dp,
 		if (fixed_mode)
 			fixed_mode->type |= DRM_MODE_TYPE_PREFERRED;
 	}
+	mutex_unlock(&dev->mode_config.mutex);
 
 	intel_panel_init(&intel_connector->panel, fixed_mode);
 	intel_connector->panel.backlight_power = intel_edp_backlight_power;
