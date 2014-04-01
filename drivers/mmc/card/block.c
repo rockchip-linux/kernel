d7aa30f29ed447b3a78f2e0afd6d463e1f610d7a
Stefan Agner <stefan@agner.ch>
2014-04-01 22:35:52 +0200

    FROMLIST: mmc: use SD/MMC host ID for block device name ID
    By using the SD/MMC host device ID as a starting point for block
    device numbering, one can reliably predict the first block device
    name (at least for the first controller). This is especially useful
    for SoCs with multiple SD/MMC host controller, where the controller
    with index 0 is connected to a eMMC device.
    
    Usually the first controller gets the first block device name ID,
    however this is not guaranteed. Also if the first controller is
    aliased as second controller and visa-versa (using device tree
    aliases), the block device name ID assignation is not ordered by
    the SD/MMC host device ID (since mmc_rescan is called in order of
    the memory mapped pheripherial addresses).
    
    BUG=chrome-os-partner:30167
    TEST=eMMC reliably gets id 0 even if SD Card is plugged in
    
    Change-Id: Ibb869d6ed982e0f21e9bc4a13caca6448b83c04b
    Signed-off-by: Stefan Agner <stefan@agner.ch>
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    (am from https://patchwork.kernel.org/patch/3925561/)
    Reviewed-on: https://chromium-review.googlesource.com/211233
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2053,7 +2053,8 @@ static struct mmc_blk_data *mmc_blk_alloc_req(struct mmc_card *card,
 	 * index anymore so we keep track of a name index.
 	 */
 	if (!subname) {
-		md->name_idx = find_first_zero_bit(name_use, max_devices);
+		md->name_idx = find_next_zero_bit(name_use, max_devices,
+				card->host->index);
 		__set_bit(md->name_idx, name_use);
 	} else
 		md->name_idx = ((struct mmc_blk_data *)
