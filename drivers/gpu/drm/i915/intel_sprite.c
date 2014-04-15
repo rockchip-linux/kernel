cedd26e54825edd98977c02bcfe268cb55751e08
Ville Syrjälä <ville.syrjala@linux.intel.com>
2014-04-15 21:41:35 +0300

    UPSTREAM: drm/i915: Wait for vblank in hsw_enable_ips()
    Now that the vblank wait is gone from intel_enable_primary_plane(),
    hsw_enable_ips() needs to do the vblank wait itself.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    
    From v3.16
    
    BUG=None
    TEST=No graphics autotest regressions
    TEST=Manual: no user experience regressions
    
    Change-Id: I85337d127c5adfa277b82609baff2269231a8c1d
    (cherry picked from commit cea165c34502052cb2aafc7e6bb36fe2036a19df)
    Signed-off-by: Joe Konno <joe.konno@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/216320
    Reviewed-by: James Ausmus <james.ausmus@intel.com>
    Tested-by: Prathyushi Nangia <prathyushi.nangia@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: James Ausmus <james.ausmus@intel.com>

--- drivers/gpu/drm/i915/intel_sprite.c
+++ drivers/gpu/drm/i915/intel_sprite.c
@@ -540,10 +540,7 @@ intel_enable_primary(struct drm_crtc *crtc)
 	 * when going from primary only to sprite only and vice
 	 * versa.
 	 */
-	if (intel_crtc->config.ips_enabled) {
-		intel_wait_for_vblank(dev, intel_crtc->pipe);
-		hsw_enable_ips(intel_crtc);
-	}
+	hsw_enable_ips(intel_crtc);
 
 	mutex_lock(&dev->struct_mutex);
 	intel_update_fbc(dev);
