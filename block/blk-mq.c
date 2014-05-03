1089d3e48967b83259d44b4ed924839ea90b9537
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2014-05-03 11:00:29 +0200

    blk-mq: revert raw locks, post pone notifier to POST_DEAD
    The blk_mq_cpu_notify_lock should be raw because some CPU down levels
    are called with interrupts off. The notifier itself calls currently one
    function that is blk_mq_hctx_notify().
    That function acquires the ctx->lock lock which is sleeping and I would
    prefer to keep it that way. That function only moves IO-requests from
    the CPU that is going offline to another CPU and it is currently the
    only one. Therefore I revert the list lock back to sleeping spinlocks
    and let the notifier run at POST_DEAD time.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1640,7 +1640,7 @@ static int blk_mq_hctx_notify(void *data, unsigned long action,
 {
 	struct blk_mq_hw_ctx *hctx = data;
 
-	if (action == CPU_DEAD || action == CPU_DEAD_FROZEN)
+	if (action == CPU_POST_DEAD)
 		return blk_mq_hctx_cpu_offline(hctx, cpu);
 
 	/*
