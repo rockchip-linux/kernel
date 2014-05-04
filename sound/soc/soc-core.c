d1d9fa02d2ec07fa2936839bc6e8879da0f06fab
Lars-Peter Clausen <lars@metafoo.de>
2014-05-04 19:18:30 +0200

    UPSTREAM: ASoC: Make soc_find_matching_codec() static
    The function is only used locally, make it static.
    
    Fixes the following warning from sparse:
    	sound/soc/soc-core.c:1644:22: warning: symbol 'soc_find_matching_codec' was not declared. Should it be static?
    
    Fixes: 3ca041ed ("ASoC: dt: Allow Aux Codecs to be specified using DT")
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Acked-By: Sebastian Reichel <sre@kernel.org>
    Signed-off-by: Mark Brown <broonie@linaro.org>
    (cherry picked from commit 6b0a0b3b4e5c8510d88824310691e47e694dd35a)
    Signed-off-by: Kevin Strasser <kevin.strasser@intel.com>
    
    BUG=chrome-os-partner:29649
    TEST=With entire patch series applied, audio playback is working
    
    Change-Id: Ib48bc35722231f06eb1ae606d48613615fc78b81
    Reviewed-on: https://chromium-review.googlesource.com/215243
    Reviewed-by: Ben Zhang <benzh@chromium.org>
    Commit-Queue: Ben Zhang <benzh@chromium.org>
    Tested-by: Ben Zhang <benzh@chromium.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1641,7 +1641,8 @@ static void soc_unregister_ac97_dai_link(struct snd_soc_pcm_runtime *rtd)
 }
 #endif
 
-struct snd_soc_codec *soc_find_matching_codec(struct snd_soc_card *card, int num)
+static struct snd_soc_codec *soc_find_matching_codec(struct snd_soc_card *card,
+	int num)
 {
 	struct snd_soc_aux_dev *aux_dev = &card->aux_dev[num];
 	struct snd_soc_codec *codec;
