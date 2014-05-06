aca4e1b5d41049e57036c9213dafd7bd5bdd8324
Lars-Peter Clausen <lars@metafoo.de>
2014-05-06 09:39:37 +0200

    UPSTREAM: ASoC: Remove card's DAI list
    Commit f0fba2ad1 ("ASoC: multi-component - ASoC Multi-Component Support") added
    a per card list that keeps track of all the DAIs that have been registered with
    the card, but the list has never been used. This patch removes it again.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>
    (cherry picked from commit 24faf7656800afa0e0d882f950502c5c03f4b7f0)
    Signed-off-by: Kevin Strasser <kevin.strasser@intel.com>
    
    BUG=chrome-os-partner:29649
    TEST=With entire patch series applied, audio playback is working
    
    Change-Id: I353c346193df16c4aa63261147af80d16f4cdbd9
    Reviewed-on: https://chromium-review.googlesource.com/215245
    Reviewed-by: Ben Zhang <benzh@chromium.org>
    Commit-Queue: Ben Zhang <benzh@chromium.org>
    Tested-by: Ben Zhang <benzh@chromium.org>

--- include/sound/soc.h
+++ include/sound/soc.h
@@ -1034,7 +1034,6 @@ struct snd_soc_card {
 	/* lists of probed devices belonging to this card */
 	struct list_head codec_dev_list;
 	struct list_head platform_dev_list;
-	struct list_head dai_dev_list;
 
 	struct list_head widgets;
 	struct list_head paths;
@@ -1220,7 +1219,6 @@ static inline void *snd_soc_pcm_get_drvdata(struct snd_soc_pcm_runtime *rtd)
 
 static inline void snd_soc_initialize_card_lists(struct snd_soc_card *card)
 {
-	INIT_LIST_HEAD(&card->dai_dev_list);
 	INIT_LIST_HEAD(&card->codec_dev_list);
 	INIT_LIST_HEAD(&card->platform_dev_list);
 	INIT_LIST_HEAD(&card->widgets);
