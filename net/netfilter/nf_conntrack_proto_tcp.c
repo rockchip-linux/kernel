9c3420e6eeeb6aceaef0454b33c04dab5899f6ea
Mathieu Olivari <mathieu@codeaurora.org>
2014-05-09 15:31:29 -0700

    CHROMIUM: netfilter: Add netfilter optional nf_ct_tcp_no_window_check
    This change added an option nf_ct_tcp_no_window_check to netfilter.
    It will be exported later for qca-nss-ecm driver to use
    
    This change is taken from OpenWRT for performance tuning
    https://dev.openwrt.org/browser/trunk/target/linux/generic/patches-2.6.39/613-netfilter_optional_tcp_window_check.patch?rev=27840
    When nf_ct_tcp_no_window_check is set, netfilter will not perform tcp
    window check and returns true directly. ECM also take this setting and
    pass it to NSS FW to skip tcp window checking for the offloaded
    connection.
    
    Source: git://codeaurora.org/quic/qsdk/oss/kernel/linux-msm
    Branch: coconut_20140924
    
    BUG=chrome-os-partner:36182
    TEST=build/boot on storm. And verified
    /proc/sys/net/netfilter/nf_conntrack_tcp_no_window_check node presents
    CQ-DEPEND=CL:245095, CL:268856
    
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>
    (cherry picked from commit fe1181ebc0e2ac4f81ff7ef3de3dba69f24174d6)
    Signed-off-by: Stephen Wang <wstephen@codeaurora.org>
    
    Change-Id: I557ef2d5b312b0a8c1f178310e6dfed6ff16fc12
    Reviewed-on: https://chromium-review.googlesource.com/245092
    Reviewed-by: Kevin Hayes <kevinhayes@google.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Commit-Queue: Kishan Kunduru <kkunduru@chromium.org>
    Tested-by: Kishan Kunduru <kkunduru@chromium.org>

--- net/netfilter/nf_conntrack_proto_tcp.c
+++ net/netfilter/nf_conntrack_proto_tcp.c
@@ -33,6 +33,9 @@
 #include <net/netfilter/ipv4/nf_conntrack_ipv4.h>
 #include <net/netfilter/ipv6/nf_conntrack_ipv6.h>
 
+/* Do not check the TCP window for incoming packets  */
+static int nf_ct_tcp_no_window_check __read_mostly = 0;
+
 /* "Be conservative in what you do,
     be liberal in what you accept from others."
     If it's non-zero, we mark only out of window RST segments as INVALID. */
@@ -514,6 +517,9 @@ static bool tcp_in_window(const struct nf_conn *ct,
 	__u32 seq, ack, sack, end, win, swin;
 	s32 receiver_offset;
 	bool res, in_recv_win;
+
+	if (nf_ct_tcp_no_window_check)
+		return true;
 
 	/*
 	 * Get the required data from the packet.
@@ -1451,6 +1457,13 @@ static struct ctl_table tcp_sysctl_table[] = {
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
+	},
+	{
+		.procname       = "nf_conntrack_tcp_no_window_check",
+		.data           = &nf_ct_tcp_no_window_check,
+		.maxlen         = sizeof(unsigned int),
+		.mode           = 0644,
+		.proc_handler   = proc_dointvec,
 	},
 	{ }
 };
