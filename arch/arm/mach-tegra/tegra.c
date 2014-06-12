857d960bb3777a24f5d22f966cedfbf6e8c562a3
Peter De Schrijver <pdeschrijver@nvidia.com>
2014-06-12 18:36:40 +0300

    UPSTREAM: soc/tegra: fuse: move APB DMA into Tegra20 fuse driver
    The Tegra20 fuse driver is the only user of tegra_apb_readl_using_dma().
    Therefore we can simply the code by incorporating the APB DMA handling into
    the driver directly. tegra_apb_writel_using_dma() is dropped because there
    are no users.
    
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    (cherry picked from commit 0d827a4343b596b71a1741328c4e5687ce654e19)
    
    TEST=compiled and booted until it tries to mount the rootfs
    BUG=chromium:393773
    
    Change-Id: If1d9b16d70d5bbb156f959452bfc691d65f1bc9b
    Signed-off-by: Rhyland Klein <rklein@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/209182
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- arch/arm/mach-tegra/tegra.c
+++ arch/arm/mach-tegra/tegra.c
@@ -44,7 +44,6 @@
 #include <asm/setup.h>
 #include <asm/trusted_foundations.h>
 
-#include "apbio.h"
 #include "board.h"
 #include "common.h"
 #include "cpuidle.h"
@@ -101,7 +100,6 @@ static void __init tegra_init_cache(void)
 static void __init tegra_init_early(void)
 {
 	of_register_trusted_foundations();
-	tegra_apb_io_init();
 	tegra_init_fuse();
 	tegra_cpu_reset_handler_init();
 	tegra_init_cache();
