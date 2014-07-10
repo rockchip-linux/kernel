b00939d3549bfc175eeff25ca87cedca282e2f05
Rhyland Klein <rklein@nvidia.com>
2014-07-09 23:25:59 -0400

    CHROMIUM: irq: tegra: Add Tegra Legacy IRQ Controller
    Move the IRQ controller code out from arch/arm and into
    drivers/irqchip. This driver will use IRQCHIP_DECLARE on
    platforms where there are no board files to initiate init.
    
    BUG=chrome-os-partner:31211
    TEST=IRQ controller comes up and interrupts are functional
    
    Change-Id: I1675e6e341db2aba01ba184fed19c6a2c2115915
    Signed-off-by: Rhyland Klein <rklein@nvidia.com>
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/220043
    Reviewed-by: David Riley <davidriley@chromium.org>

--- drivers/irqchip/irq-tegra.c
+++ drivers/irqchip/irq-tegra.c
@@ -19,6 +19,7 @@
 
 #include <linux/cpu_pm.h>
 #include <linux/interrupt.h>
+#include <linux/init.h>
 #include <linux/io.h>
 #include <linux/irqchip/arm-gic.h>
 #include <linux/irq.h>
@@ -27,10 +28,10 @@
 #include <linux/of.h>
 #include <linux/syscore_ops.h>
 
+#include <soc/tegra/fuse.h>
 #include <soc/tegra/iomap.h>
 
-#include "board.h"
-#include <soc/tegra/fuse.h>
+#include "irqchip.h"
 
 #define ICTLR_CPU_IEP_VFIQ	0x08
 #define ICTLR_CPU_IEP_FIR	0x14
@@ -284,16 +285,19 @@ static const struct of_device_id gic_matches[] = {
 	{ }
 };
 
-void __init tegra_init_irq(void)
+static int __init tegra_irq_init(struct device_node *np,
+				 struct device_node *parent)
 {
 	unsigned int max_ictlrs = ARRAY_SIZE(ictlr_regs), i;
 	const struct of_device_id *match;
-	struct device_node *np;
+	struct device_node *gic;
 	struct resource res;
 
-	np = of_find_matching_node_and_match(NULL, ictlr_matches, &match);
 	if (np) {
-		const struct tegra_ictlr_soc *soc = match->data;
+		const struct tegra_ictlr_soc *soc;
+
+		match = of_match_node(ictlr_matches, np);
+		soc = match->data;
 
 		for (i = 0; i < soc->num_ictlrs; i++) {
 			if (of_address_to_resource(np, i, &res) < 0)
@@ -307,7 +311,6 @@ void __init tegra_init_irq(void)
 		     i, soc->num_ictlrs);
 
 		max_ictlrs = soc->num_ictlrs;
-		of_node_put(np);
 	} else {
 		/*
 		 * If no matching device node was found, fall back to using
@@ -324,12 +327,12 @@ void __init tegra_init_irq(void)
 
 	memset(&res, 0, sizeof(res));
 
-	np = of_find_matching_node(NULL, gic_matches);
-	if (np) {
-		if (of_address_to_resource(np, 0, &res) < 0)
+	gic = of_find_matching_node(NULL, gic_matches);
+	if (gic) {
+		if (of_address_to_resource(gic, 0, &res) < 0)
 			WARN(1, "GIC registers are missing from DT\n");
 
-		of_node_put(np);
+		of_node_put(gic);
 	}
 
 	if (res.start == 0 || res.end == 0) {
@@ -366,4 +369,36 @@ void __init tegra_init_irq(void)
 	gic_arch_extn.flags = IRQCHIP_MASK_ON_SUSPEND;
 
 	tegra114_gic_cpu_pm_registration();
+
+	pr_info("Tegra Legacy Interrupt Controller initialized.\n");
+
+	return 0;
 }
+#ifdef CONFIG_ARCH_TEGRA_132_SOC
+IRQCHIP_DECLARE(tegra30_irq_chip, "nvidia,tegra30-ictlr", tegra_irq_init);
+IRQCHIP_DECLARE(tegra20_irq_chip, "nvidia,tegra20-ictlr", tegra_irq_init);
+#else
+void __init tegra_init_irq(void)
+{
+	const struct of_device_id *match;
+	struct device_node *np, *parent;
+
+	np = of_find_matching_node_and_match(NULL, ictlr_matches, &match);
+	if (!np) {
+		pr_err("Can't find matching ictrl node\n");
+		return;
+	}
+
+	parent = of_get_parent(np);
+	if (!parent) {
+		pr_err("Can't get ictrl parent\n");
+		of_node_put(np);
+		return;
+	};
+
+	tegra_irq_init(np, parent);
+
+	of_node_put(parent);
+	of_node_put(np);
+}
+#endif
