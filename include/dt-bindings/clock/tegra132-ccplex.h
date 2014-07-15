9e728e06af1ce0f65bdb9b95bb4d140c46483b26
Peter De Schrijver <pdeschrijver@nvidia.com>
2014-07-15 18:24:34 +0300

    CHROMIUM: clk: tegra: add nvidia,tegra132-ccplex-clk binding
    Tegra132 has a few new clocks for the CPU complex (ccplex).
    
    Change-Id: I6a2be1b02d9399c80a8e54b4c26d8292fc31c8e0
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Signed-off-by: David Riley <davidriley@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/211356
    Reviewed-by: Rhyland Klein <rklein@nvidia.com>
    Reviewed-by: Olof Johansson <olofj@chromium.org>

---
+++ include/dt-bindings/clock/tegra132-ccplex.h
@@ -0,0 +1,12 @@
+/*
+ * This header provides constants for the binding nvidia,tegra132-ccplex-clk
+ */
+
+#ifndef __DT_BINDINGS_CLOCK_TEGRA132_CCPLEX_H
+#define __DT_BINDINGS_CLOCK_TEGRA132_CCPLEX_H
+
+#define TEGRA132_CCPLEX_CCLK_G 1
+#define TEGRA132_PLL_X 2
+#define TEGRA132_CCPLEX_CLK_MAX 3
+
+#endif
