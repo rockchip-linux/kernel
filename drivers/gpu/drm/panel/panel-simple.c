0ac2a796e597fca9b4e1b621776c7a24fdbcd4ed
Stéphane Marchesin <marcheu@chromium.org>
2014-07-15 13:33:25 -0700

    CHROMIUM: drm/panel: Add support for CMN N116BGE-EA2 panel
    This panel can be supported by the simple panel driver.
    
    BUG=chromium:385270
    TEST=builds
    
    Change-Id: Ia5cd66a1af2110a6a3fc7ef932f8d7f5745374d5
    Reviewed-on: https://chromium-review.googlesource.com/208450
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -367,6 +367,29 @@ static const struct panel_desc chunghwa_claa101wb01 = {
 	},
 };
 
+static const struct drm_display_mode cmn_n116bgeea2_mode = {
+	.clock = 76420,
+	.hdisplay = 1366,
+	.hsync_start = 1366 + 136,
+	.hsync_end = 1366 + 136 + 30,
+	.htotal = 1366 + 136 + 30 + 60,
+	.vdisplay = 768,
+	.vsync_start = 768 + 8,
+	.vsync_end = 768 + 8 + 12,
+	.vtotal = 768 + 8 + 12 + 12,
+	.vrefresh = 60,
+};
+
+static const struct panel_desc cmn_n116bgeea2 = {
+	.modes = &cmn_n116bgeea2_mode,
+	.num_modes = 1,
+	.bpc = 6,
+	.size = {
+		.width = 256,
+		.height = 144,
+	},
+};
+
 static const struct drm_display_mode samsung_ltn101nt05_mode = {
 	.clock = 54030,
 	.hdisplay = 1024,
@@ -400,6 +423,9 @@ static const struct of_device_id platform_of_match[] = {
 	}, {
 		.compatible = "chunghwa,claa101wb01",
 		.data = &chunghwa_claa101wb01
+	}, {
+		.compatible = "cnm,n116bgeea2",
+		.data = &cmn_n116bgeea2
 	}, {
 		.compatible = "samsung,ltn101nt05",
 		.data = &samsung_ltn101nt05,
