ca76ae46bd8fb233aa2188adc08baeaaa0361902
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-07-29 13:47:11 +0200

    BACKPORT: drm: Add drm_plane/connector_index
    In the atomic state we'll have an array of states for crtcs, planes
    and connectors and need to be able to at them by their index. We
    already have a drm_crtc_index function so add the missing ones for
    planes and connectors.
    
    If it later on turns out that the list walking is too expensive we can
    add the index to the relevant modeset objects.
    
    Rob Clark doesn't like the loops too much, but we can always add an
    obj->idx parameter later on. And for now reiterating is actually safer
    since nowadays we have hotpluggable connectors (thanks to DP MST).
    
    v2: Fix embarrassing copypasta fail in kerneldoc and header
    declarations, spotted by Matt Roper.
    
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 10f637bf292ba501f9b9e9df6dfe21d8fa521fbd)
    
    Change-Id: I4db789db7057a5a1d4c3dde035cd108debe40fb4
    Reviewed-on: https://chromium-review.googlesource.com/268549
    Trybot-Ready: Haixia Shi <hshi@chromium.org>
    Tested-by: Haixia Shi <hshi@chromium.org>
    Commit-Queue: Haixia Shi <hshi@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    (cherry picked from commit ee69a087bb5089bad5662da037ea5ed7dc77e351)
    Reviewed-on: https://chromium-review.googlesource.com/269606

--- drivers/gpu/drm/drm_crtc.c
+++ drivers/gpu/drm/drm_crtc.c
@@ -1098,7 +1098,30 @@ int drm_crtc_set_property(struct drm_crtc *crtc,
 }
 EXPORT_SYMBOL(drm_crtc_set_property);
 
-/*
+/**
+ * drm_connector_index - find the index of a registered connector
+ * @connector: connector to find index for
+ *
+ * Given a registered connector, return the index of that connector within a DRM
+ * device's list of connectors.
+ */
+unsigned int drm_connector_index(struct drm_connector *connector)
+{
+	unsigned int index = 0;
+	struct drm_connector *tmp;
+
+	list_for_each_entry(tmp, &connector->dev->mode_config.connector_list, head) {
+		if (tmp == connector)
+			return index;
+
+		index++;
+	}
+
+	BUG();
+}
+EXPORT_SYMBOL(drm_connector_index);
+
+/**
  * drm_mode_remove - remove and free a mode
  * @connector: connector list to modify
  * @mode: mode to remove
@@ -1666,6 +1689,29 @@ int drm_plane_set_property(struct drm_plane *plane,
 	return 0;
 }
 EXPORT_SYMBOL(drm_plane_set_property);
+
+/**
+ * drm_plane_index - find the index of a registered plane
+ * @plane: plane to find index for
+ *
+ * Given a registered plane, return the index of that CRTC within a DRM
+ * device's list of planes.
+ */
+unsigned int drm_plane_index(struct drm_plane *plane)
+{
+	unsigned int index = 0;
+	struct drm_plane *tmp;
+
+	list_for_each_entry(tmp, &plane->dev->mode_config.plane_list, head) {
+		if (tmp == plane)
+			return index;
+
+		index++;
+	}
+
+	BUG();
+}
+EXPORT_SYMBOL(drm_plane_index);
 
 /**
  * drm_plane_force_disable - Forcibly disable a plane
