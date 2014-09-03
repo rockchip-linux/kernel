59759999e14f0076c17e977baeeccc3bbe91fe86
Ulf Hansson <ulf.hansson@linaro.org>
2014-09-03 12:52:28 +0200

    UPSTREAM: ARM: s3c64xx: Leave disabling of unused PM domains to genpd
    Since genpd at late init, will try to disable unused PM domains we
    don't need to do it from the machine specific code as well.
    
    BUG=chrome-os-partner:30167
    TEST=Build and boot; future power domain patches can work
    
    Change-Id: I35231a9f42eda3537a7c665aa73ac1ec7726b01b
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Reviewed-by: Kevin Hilman <khilman@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    (cherry picked from git.kernel.org rafael/linux-pm.git linux-next
     commit d40fce7e625dcecb196b1e4dba4dde19fdee919c)
    Reviewed-on: https://chromium-review.googlesource.com/217909
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>

--- arch/arm/mach-s3c64xx/pm.c
+++ arch/arm/mach-s3c64xx/pm.c
@@ -348,10 +348,3 @@ static __init int s3c64xx_pm_initcall(void)
 	return 0;
 }
 arch_initcall(s3c64xx_pm_initcall);
-
-int __init s3c64xx_pm_late_initcall(void)
-{
-	pm_genpd_poweroff_unused();
-
-	return 0;
-}
