b5904476b4997d40c1b9328013bfd1ffce431bd0
Andrew Bresticker <abrestic@chromium.org>
2014-09-23 15:04:19 -0700

    CHROMIUM: usb: dwc2: Add support for IMG Pistachio
    Add support for the DWC2 controller found on the Imagination Pistachio
    SoC.
    
    Note: these parameters are only correct for the DWC2 controller present
    in the Pistachio FPGA.  It is expected that they will be different in
    the final silicon.
    
    BUG=chrome-os-partner:30840
    TEST=Build and boot on FPGA
    
    Change-Id: Ibea1059f7a0c35d208adab1c6bf86e5d2f2cc11b
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/220065
    Reviewed-by: Doug Anderson <dianders@chromium.org>

--- drivers/usb/dwc2/platform.c
+++ drivers/usb/dwc2/platform.c
@@ -105,6 +105,34 @@ static const struct dwc2_core_params params_rk3066 = {
 	.uframe_sched			= -1,
 };
 
+static const struct dwc2_core_params params_pistachio = {
+	.otg_cap			= 0,	/* HNP/SRP capable */
+	.otg_ver			= 0,	/* 1.3 */
+	.dma_enable			= -1,
+	.dma_desc_enable		= -1,
+	.speed				= -1,
+	.enable_dynamic_fifo		= -1,
+	.en_multiple_tx_fifo		= -1,
+	.host_rx_fifo_size		= 1536,	/* 1536 DWORDs */
+	.host_nperio_tx_fifo_size	= 1536,	/* 1536 DWORDs */
+	.host_perio_tx_fifo_size	= 1536,	/* 1536 DWORDs */
+	.max_transfer_size		= -1,
+	.max_packet_count		= -1,
+	.host_channels			= -1,
+	.phy_type			= -1,
+	.phy_utmi_width			= 8,	/* 8 bits */
+	.phy_ulpi_ddr			= -1,
+	.phy_ulpi_ext_vbus		= -1,
+	.i2c_enable			= -1,
+	.ulpi_fs_ls			= -1,
+	.host_support_fs_ls_low_power	= -1,
+	.host_ls_low_power_phy_clk	= -1,
+	.ts_dline			= -1,
+	.reload_ctl			= -1,
+	.ahbcfg				= -1,
+	.uframe_sched			= -1,
+};
+
 /**
  * dwc2_driver_remove() - Called when the DWC_otg core is unregistered with the
  * DWC_otg driver
@@ -127,6 +155,7 @@ static int dwc2_driver_remove(struct platform_device *dev)
 
 static const struct of_device_id dwc2_of_match_table[] = {
 	{ .compatible = "brcm,bcm2835-usb", .data = &params_bcm2835 },
+	{ .compatible = "img,pistachio-usb", .data = &params_pistachio },
 	{ .compatible = "rockchip,rk3066-usb", .data = &params_rk3066 },
 	{ .compatible = "snps,dwc2", .data = NULL },
 	{},
