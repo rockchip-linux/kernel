c29c5a3037e18815937d8af664738e499ada94d1
Mark Yao <yzq@rock-chips.com>
2014-10-08 17:11:15 +0800

    CHROMIUM: drm/rockchip: Add GEM create ioctl support
    Rockchip Socs have GPU, we need allocate GPU accelerated buffers.
    So add special ioctls GEM_CREATE/GEM_MAP_OFFSET to support
    accelerated buffers.
    
    Signed-off-by: Mark Yao <mark.yao@rock-chips.com>
    
    BUG=chromium:399935
    TEST=With rest of patch set, can boot to UI on eDP
    
    Change-Id: Ia4b13798aac97d16214da7a75a2479e6e334313e
    Reviewed-on: https://chromium-review.googlesource.com/222153
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>

--- drivers/gpu/drm/rockchip/rockchip_drm_drv.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_drv.c
@@ -24,6 +24,8 @@
 #include <linux/of_graph.h>
 #include <linux/component.h>
 
+#include <drm/rockchip_drm.h>
+
 #include "rockchip_drm_drv.h"
 #include "rockchip_drm_fb.h"
 #include "rockchip_drm_fbdev.h"
@@ -166,6 +168,14 @@ void rockchip_drm_lastclose(struct drm_device *dev)
 	drm_fb_helper_restore_fbdev_mode_unlocked(&priv->fbdev_helper);
 }
 
+static const struct drm_ioctl_desc rockchip_ioctls[] = {
+	DRM_IOCTL_DEF_DRV(ROCKCHIP_GEM_CREATE, rockchip_gem_create_ioctl,
+			  DRM_UNLOCKED | DRM_AUTH),
+	DRM_IOCTL_DEF_DRV(ROCKCHIP_GEM_MAP_OFFSET,
+			  rockchip_gem_map_offset_ioctl,
+			  DRM_UNLOCKED | DRM_AUTH),
+};
+
 static const struct file_operations rockchip_drm_driver_fops = {
 	.owner = THIS_MODULE,
 	.open = drm_open,
@@ -210,6 +220,8 @@ static struct drm_driver rockchip_drm_driver = {
 	.atomic_commit		= drm_atomic_commit,
 	.atomic_end		= drm_atomic_end,
 	.atomic_funcs		= &drm_atomic_funcs,
+	.ioctls			= rockchip_ioctls,
+	.num_ioctls		= ARRAY_SIZE(rockchip_ioctls),
 	.fops			= &rockchip_drm_driver_fops,
 	.name	= DRIVER_NAME,
 	.desc	= DRIVER_DESC,
