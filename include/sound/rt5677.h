0ede0c8786365b50b9c1f26a75c9934972ce24e2
Anatol Pomozov <anatol.pomozov@gmail.com>
2014-10-16 11:24:14 -0700

    FROMLIST: ASoC: rt5677: add GPIO IRQ support
    This allows to enable Mic Jack detection feature
    
    BUG=chrome-os-partner:30793
    
    Author: Oder Chiou <oder_chiou@realtek.com>
    Modified-by: Anatol Pomozov <anatol.pomozov@gmail.com>
    Signed-off-by: Anatol Pomozov <anatol.pomozov@gmail.com>
    
    Conflicts:
    	Documentation/devicetree/bindings/sound/rt5677.txt
    	include/sound/rt5677.h
    	sound/soc/codecs/rt5677.c
    
    Change-Id: I9794354c1fab1f1c0911f93cd4ab6b983317cb4e
    Reviewed-on: https://chromium-review.googlesource.com/224143
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-by: Anatol Pomazau <anatol@google.com>
    Commit-Queue: Anatol Pomazau <anatol@google.com>
    Tested-by: Anatol Pomazau <anatol@google.com>

--- include/sound/rt5677.h
+++ include/sound/rt5677.h
@@ -32,6 +32,13 @@ struct rt5677_platform_data {
 	u8 gpio_config[6];
 	/* Asynchronous Sample Rate Converter can be optionally enabled */
 	bool asrc_en;
+
+	/* jd1 can select 0 ~ 3 as OFF, GPIO1, GPIO2 and GPIO3 respectively */
+	unsigned int jd1_gpio;
+	/* jd2 and jd3 can select 0 ~ 3 as
+		OFF, GPIO4, GPIO5 and GPIO6 respectively */
+	unsigned int jd2_gpio;
+	unsigned int jd3_gpio;
 };
 
 #endif
