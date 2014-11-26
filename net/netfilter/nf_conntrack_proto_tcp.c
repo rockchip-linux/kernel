6431d2d1c4d21787d35700d595496e3b906c646f
Murat Sezgin <msezgin@codeaurora.org>
2014-11-26 09:28:27 -0800

    CHROMIUM: netfilter: Export TCP parameters for HW acceleration connection manager.
    nf_ct_tcp_be_liberal and nf_ct_tcp_no_window_check parameters were
    exported for qca-nss-ecm to use.
    
    Source: git://codeaurora.org/quic/qsdk/oss/kernel/linux-msm
    Branch: coconut_20140924
    
    BUG=chrome-os-partner:36182
    TEST=build/boot on storm.
    CQ-DEPEND=CL:245095, CL:268856
    
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>
    (cherry picked from commit d881c6e556cd04a53046c81bf6115cd9d6c9697c)
    Signed-off-by: Stephen Wang <wstephen@codeaurora.org>
    Change-Id: Ifdaadc27fb359bc6b003713f5c18d35d18d42e13
    Reviewed-on: https://chromium-review.googlesource.com/245101
    Reviewed-by: Kevin Hayes <kevinhayes@google.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Commit-Queue: Kishan Kunduru <kkunduru@chromium.org>
    Tested-by: Kishan Kunduru <kkunduru@chromium.org>

--- net/netfilter/nf_conntrack_proto_tcp.c
+++ net/netfilter/nf_conntrack_proto_tcp.c
@@ -34,12 +34,14 @@
 #include <net/netfilter/ipv6/nf_conntrack_ipv6.h>
 
 /* Do not check the TCP window for incoming packets  */
-static int nf_ct_tcp_no_window_check __read_mostly = 0;
+int nf_ct_tcp_no_window_check __read_mostly = 0;
+EXPORT_SYMBOL_GPL(nf_ct_tcp_no_window_check);
 
 /* "Be conservative in what you do,
     be liberal in what you accept from others."
     If it's non-zero, we mark only out of window RST segments as INVALID. */
-static int nf_ct_tcp_be_liberal __read_mostly = 0;
+int nf_ct_tcp_be_liberal __read_mostly = 0;
+EXPORT_SYMBOL_GPL(nf_ct_tcp_be_liberal);
 
 /* If it is set to zero, we disable picking up already established
    connections. */
