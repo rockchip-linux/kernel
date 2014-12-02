4e9021a2c6aa14a93b12b907c94d30b210e05554
Ezequiel Garcia <ezequiel.garcia@imgtec.com>
2014-12-02 06:37:01 -0300

    FROMLIST: mtd: nand: Check length of ID before reading bits per cell
    The table-based NAND identification currently reads the number
    of bits per cell from the 3rd byte of the extended ID. This is done
    for the so-called 'full ID' devices; i.e. devices that have a known
    length ID.
    
    However, if the ID length is shorter than three, there's no 3rd byte,
    and so it's wrong to read the bits per cell from there. Fix this by
    adding a check for the ID length.
    
    (am from https://patchwork.ozlabs.org/patch/416851/)
    
    BUG=chrome-os-partner:30840
    TEST=Tested on Pistachio BuB
    
    Change-Id: Ibeae1c88a6645bb4f73ed9014def5d86ba169e1f
    Signed-off-by: Ezequiel Garcia <ezequiel.garcia@imgtec.com>
    Reviewed-on: https://chromium-review.googlesource.com/237420
    Reviewed-by: Daniel Ehrenberg <dehrenberg@chromium.org>
    Tested-by: Ionela Voinescu <ionela.voinescu@imgtec.com>
    Reviewed-by: James Hartley <james.hartley@imgtec.com>
    Commit-Queue: James Hartley <james.hartley@imgtec.com>

--- drivers/mtd/nand/nand_base.c
+++ drivers/mtd/nand/nand_base.c
@@ -3453,7 +3453,9 @@ static bool find_full_id_nand(struct mtd_info *mtd, struct nand_chip *chip,
 		mtd->erasesize = type->erasesize;
 		mtd->oobsize = type->oobsize;
 
-		chip->bits_per_cell = nand_get_bits_per_cell(id_data[2]);
+		if (type->id_len > 2)
+			chip->bits_per_cell =
+				nand_get_bits_per_cell(id_data[2]);
 		chip->chipsize = (uint64_t)type->chipsize << 20;
 		chip->options |= type->options;
 		chip->ecc_strength_ds = NAND_ECC_STRENGTH(type);
