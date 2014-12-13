54c623c576165bb6182fb5d5d2d50056cd5f9ba1
karam.lee <karam.lee@lge.com>
2014-12-12 16:56:47 -0800

    BACKPORT: zram: remove bio parameter from zram_bvec_rw()
    (cherry-pick from commit b627cff3d308d3ccb3ec73a89260f5c7872e46a4)
    
    Recently rw_page block device operation has been added.  This patchset
    implements rw_page operation for zram block device and does some clean-up.
    
    This patch (of 3):
    
    Remove an unnecessary parameter(bio) from zram_bvec_rw() and
    zram_bvec_read().  zram_bvec_read() doesn't use a bio parameter, so remove
    it.  zram_bvec_rw() calls a read/write operation not using bio, so a rw
    parameter replaces a bio parameter.
    
    Bug: 25951511
    
    Change-Id: I6aea368206cad8032a2deff25d2638df871b7629
    Signed-off-by: karam.lee <karam.lee@lge.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Acked-by: Jerome Marchand <jmarchan@redhat.com>
    Cc: Matthew Wilcox <matthew.r.wilcox@intel.com>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: <seungho1.park@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -452,7 +452,7 @@ static int zram_decompress_page(struct zram *zram, char *mem, u32 index)
 }
 
 static int zram_bvec_read(struct zram *zram, struct bio_vec *bvec,
-			  u32 index, int offset, struct bio *bio)
+			  u32 index, int offset)
 {
 	int ret;
 	struct page *page;
@@ -644,14 +644,13 @@ out:
 }
 
 static int zram_bvec_rw(struct zram *zram, struct bio_vec *bvec, u32 index,
-			int offset, struct bio *bio)
+			int offset, int rw)
 {
 	int ret;
-	int rw = bio_data_dir(bio);
 
 	if (rw == READ) {
 		atomic64_inc(&zram->stats.num_reads);
-		ret = zram_bvec_read(zram, bvec, index, offset, bio);
+		ret = zram_bvec_read(zram, bvec, index, offset);
 	} else {
 		atomic64_inc(&zram->stats.num_writes);
 		ret = zram_bvec_write(zram, bvec, index, offset);
@@ -852,7 +851,7 @@ out:
 
 static void __zram_make_request(struct zram *zram, struct bio *bio)
 {
-	int i, offset;
+	int i, offset, rw;
 	u32 index;
 	struct bio_vec *bvec;
 
@@ -865,6 +864,7 @@ static void __zram_make_request(struct zram *zram, struct bio *bio)
 		return;
 	}
 
+	rw = bio_data_dir(bio);
 	bio_for_each_segment(bvec, bio, i) {
 		int max_transfer_size = PAGE_SIZE - offset;
 
@@ -879,15 +879,15 @@ static void __zram_make_request(struct zram *zram, struct bio *bio)
 			bv.bv_len = max_transfer_size;
 			bv.bv_offset = bvec->bv_offset;
 
-			if (zram_bvec_rw(zram, &bv, index, offset, bio) < 0)
+			if (zram_bvec_rw(zram, &bv, index, offset, rw) < 0)
 				goto out;
 
 			bv.bv_len = bvec->bv_len - max_transfer_size;
 			bv.bv_offset += max_transfer_size;
-			if (zram_bvec_rw(zram, &bv, index + 1, 0, bio) < 0)
+			if (zram_bvec_rw(zram, &bv, index + 1, 0, rw) < 0)
 				goto out;
 		} else
-			if (zram_bvec_rw(zram, bvec, index, offset, bio) < 0)
+			if (zram_bvec_rw(zram, bvec, index, offset, rw) < 0)
 				goto out;
 
 		update_position(&index, &offset, bvec);
