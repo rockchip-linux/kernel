601b85ac353dca577f5528f9e4a8a352d8a52fed
Amitkumar Karwar <akarwar@marvell.com>
2015-01-27 01:41:37 -0800

    UPSTREAM: mwifiex: add wakeup timer based recovery mechanism
    If host fails to wakeup the firmware, we will trigger card reset
    after 3 second timeout.
    
    Signed-off-by: Cathy Luo <cluo@marvell.com>
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    
    BUG=chrome-os-partner:34110
    TEST=With all patches in this series applied, ran suspend_stress
    test to pass 10000 iterations on Lucas2
    
    Change-Id: I878a7199829c2af7af69aa532caa842edbbbf26a
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Reviewed-on: https://chromium-review.googlesource.com/239146
    Reviewed-by: Paul Stewart <pstew@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/243504

--- drivers/net/wireless-3.8/mwifiex/main.c
+++ drivers/net/wireless-3.8/mwifiex/main.c
@@ -226,6 +226,7 @@ process_start:
 		    (is_command_pending(adapter) ||
 		     !mwifiex_wmm_lists_empty(adapter))) {
 			adapter->pm_wakeup_fw_try = true;
+			mod_timer(&adapter->wakeup_timer, jiffies + (HZ*3));
 			adapter->if_ops.wakeup(adapter);
 			continue;
 		}
@@ -233,6 +234,7 @@ process_start:
 		if (IS_CARD_RX_RCVD(adapter)) {
 			adapter->data_received = false;
 			adapter->pm_wakeup_fw_try = false;
+			del_timer_sync(&adapter->wakeup_timer);
 			if (adapter->ps_state == PS_STATE_SLEEP) {
 				if (adapter->iface_type == MWIFIEX_PCIE &&
 				    adapter->if_ops.enable_int)
