aa99396346ed2537d9166824d6e0ecfab02ed5ac
Amitkumar Karwar <akarwar@marvell.com>
2015-02-04 01:22:55 -0800

    CHROMIUM: mwifiex: advertise low priority scan feature
    Low priority scan handling code which delays or aborts scan
    operation based on Tx traffic is removed recently. The reason
    is firmware already takes care of it in our new feature scan
    channel gap. Hence we should advertise low priority scan
    support to cfg80211.
    
    This patch fixes a problem in which OBSS scan request from
    wpa_supplicant was being rejected by cfg80211.
    
    BUG=none
    TEST=Ran network_WiFi_OverlappingBSSScan test successfully on
    Nyan Blaze
    
    Change-Id: I30ad9a90071552fdc9d00dc657e63db58d7aed56
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Reviewed-on: https://chromium-review.googlesource.com/246052
    Reviewed-by: Paul Stewart <pstew@chromium.org>

--- drivers/net/wireless-3.8/mwifiex/cfg80211.c
+++ drivers/net/wireless-3.8/mwifiex/cfg80211.c
@@ -2604,6 +2604,7 @@ int mwifiex_register_cfg80211(struct mwifiex_adapter *adapter)
 
 	wiphy->features |= NL80211_FEATURE_HT_IBSS |
 			   NL80211_FEATURE_INACTIVITY_TIMER |
+			   NL80211_FEATURE_LOW_PRIORITY_SCAN |
 			   NL80211_FEATURE_NEED_OBSS_SCAN;
 
 	/* Reserve space for mwifiex specific private data for BSS */
