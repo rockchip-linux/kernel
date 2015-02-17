2a55f7be521942cea6dbca965f4949f48e14c026
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2015-02-17 16:08:26 +0100

    FROMLIST: media/v4l2-ctrls: Always run s_ctrl on volatile ctrls
    Volatile controls can change their value outside the v4l-ctrl framework.
    We should ignore the cached written value of the ctrl when evaluating if
    we should run s_ctrl.
    
    Signed-off-by: Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
    
    (am from https://patchwork.linuxtv.org/patch/28279)
    
    Signed-off-by: Pawel Osciak <posciak@chromium.org>
    
    BUG=chromium:457481
    TEST=veaunittest using volatile controls for keyframe request
    
    Change-Id: If891ca9bf9801d6b2515dc666f7f5a39abfaa03a
    Reviewed-on: https://chromium-review.googlesource.com/251490
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Commit-Queue: Pawel Osciak <posciak@chromium.org>
    Trybot-Ready: Pawel Osciak <posciak@chromium.org>
    Tested-by: Pawel Osciak <posciak@chromium.org>

--- drivers/media/v4l2-core/v4l2-ctrls.c
+++ drivers/media/v4l2-core/v4l2-ctrls.c
@@ -1569,6 +1569,17 @@ static int cluster_changed(struct v4l2_ctrl *master)
 
 		if (ctrl == NULL)
 			continue;
+
+		if (ctrl->flags & V4L2_CTRL_FLAG_VOLATILE) {
+			/*
+			 * Set has_changed to false to avoid generating
+			 * the event V4L2_EVENT_CTRL_CH_VALUE
+			 */
+			ctrl->has_changed = false;
+			changed = true;
+			continue;
+		}
+
 		if (ctrl->store)
 			ptr = ctrl->p_stores[ctrl->store - 1];
 		else
