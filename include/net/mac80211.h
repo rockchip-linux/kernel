15d2f46b556a6ee4b8cb557031feee587b5df82a
Sujith Manoharan <c_manoha@qca.qualcomm.com>
2015-02-25 00:12:09 +0530

    UPSTREAM: mac80211: Move IEEE80211_TX_CTL_PS_RESPONSE
    Move IEEE80211_TX_CTL_PS_RESPONSE to info->control.flags since
    this is used only in the TX path (by ath9k). This frees up
    a bit which can be used for other purposes.
    
    Signed-off-by: Sujith Manoharan <c_manoha@qca.qualcomm.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    (cherry picked from commit 6b127c71fbdd3daacfd8b9f80b8e6ebfb70a889e)
    (source: https://github.com/kvalo/ath/tree/ath-qca)
    
    TEST=build/boot on storm suceeded. Setup Storm board as AP using hostapd and
    connected to the board using another device. Device was able to connect to
    the internet and load multiple websites.
    
    Change-Id: I33f971a2ba8711911e5220d9f275a74580087c8d
    Reviewed-on: https://chromium-review.googlesource.com/253506
    Reviewed-by: Toshi Kikuchi <toshik@chromium.org>
    Tested-by: Toshi Kikuchi <toshik@chromium.org>
    Commit-Queue: Toshi Kikuchi <toshik@chromium.org>

--- include/net/mac80211.h
+++ include/net/mac80211.h
@@ -470,7 +470,7 @@ struct ieee80211_bss_conf {
  *	would be fragmented by size (this is optional, only used for
  *	monitor injection).
  * @IEEE80211_TX_CTL_PS_RESPONSE: This frame is a response to a poll
- *	frame (PS-Poll or uAPSD).
+ * 	frame (PS-Poll or uAPSD).
  *
  * Note: If you have to add new flags to the enumeration, then don't
  *	 forget to update %IEEE80211_TX_TEMPORARY_FLAGS when necessary.
@@ -506,7 +506,7 @@ enum mac80211_tx_info_flags {
 	IEEE80211_TX_STATUS_EOSP		= BIT(28),
 	IEEE80211_TX_CTL_USE_MINRATE		= BIT(29),
 	IEEE80211_TX_CTL_DONTFRAG		= BIT(30),
-	IEEE80211_TX_CTL_PS_RESPONSE		= BIT(31),
+	IEEE80211_TX_CTL_PS_RESPONSE            = BIT(31),
 };
 
 #define IEEE80211_TX_CTL_STBC_SHIFT		23
