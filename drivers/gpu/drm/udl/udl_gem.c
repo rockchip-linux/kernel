c379a6da607bf36b7e4fc66a9913cdc1d2b39c21
Haixia Shi <hshi@chromium.org>
2015-03-17 17:57:17 -0700

    CHROMIUM: drm/udl: fix a NULL pointer reference in udl_gem_free_object().
    Previously this function had a NULL pointer check for gem->map_list.map, but
    that line was refactored after commit 0de23977cfeb5b357ec884ba15417ae118ff9e9bb
    ("drm/gem: convert to new unified vma manager").
    
    After the refactor it is still necessasry to check that the vma manager is not
    NULL because udl_gem_free_object() may come in after the vma manager is destroyed.
    
    Signed-off-by: Haixia Shi <hshi@chromium.org>
    BUG=chromium:468050
    TEST=verify that unplug UDL works correctly on samus
    
    Change-Id: I9caea6a2badf14de42945be01e54927fc52624f1
    Reviewed-on: https://chromium-review.googlesource.com/260687
    Commit-Queue: Haixia Shi <hshi@chromium.org>
    Trybot-Ready: Haixia Shi <hshi@chromium.org>
    Tested-by: Haixia Shi <hshi@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/udl/udl_gem.c
+++ drivers/gpu/drm/udl/udl_gem.c
@@ -208,7 +208,8 @@ void udl_gem_free_object(struct drm_gem_object *gem_obj)
 	if (obj->pages)
 		udl_gem_put_pages(obj);
 
-	drm_gem_free_mmap_offset(gem_obj);
+	if (gem_obj->dev->vma_offset_manager)
+		drm_gem_free_mmap_offset(gem_obj);
 }
 
 /* the dumb interface doesn't work with the GEM straight MMAP
