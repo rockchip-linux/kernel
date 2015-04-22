c031193aab7e380f4f1c724ce4e566fb903da27c
Zach Reizner <zachr@google.com>
2015-04-22 11:16:58 -0700

    drm/i915: implement dmabuf mmap
    VGEM depends on the dmabuf mmap function for mmaping of imported
    buffers. This implementation means VGEM can mmap i915 display buffers.
    
    BUG=chromium:475633
    TEST=on link run: vgem_fb_test
    
    Change-Id: I2e964c437c633ca09481b3fd93c721449a0ec3f5
    Signed-off-by: Zach Reizner <zachr@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/267111
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Zach Reizner <zachr@chromium.org>
    Tested-by: Zach Reizner <zachr@chromium.org>

--- drivers/gpu/drm/i915/i915_gem_dmabuf.c
+++ drivers/gpu/drm/i915/i915_gem_dmabuf.c
@@ -197,7 +197,21 @@ static void i915_gem_dmabuf_kunmap(struct dma_buf *dma_buf, unsigned long page_n
 
 static int i915_gem_dmabuf_mmap(struct dma_buf *dma_buf, struct vm_area_struct *vma)
 {
-	return -EINVAL;
+	struct drm_i915_gem_object *obj = dma_buf_to_obj(dma_buf);
+	struct drm_device *dev = obj->base.dev;
+
+	if (obj->base.size < vma->vm_end - vma->vm_start)
+		return -EINVAL;
+
+	vma->vm_flags |= VM_IO | VM_PFNMAP | VM_DONTEXPAND | VM_DONTDUMP;
+	vma->vm_ops = dev->driver->gem_vm_ops;
+	vma->vm_private_data = &obj->base;
+	vma->vm_page_prot =
+		pgprot_writecombine(vm_get_page_prot(vma->vm_flags));
+
+	vma->vm_ops->open(vma);
+
+	return 0;
 }
 
 static int i915_gem_begin_cpu_access(struct dma_buf *dma_buf, size_t start, size_t length, enum dma_data_direction direction)
