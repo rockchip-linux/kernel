d479d781a2161a08a99ceaf74e5b2c4b3c87a3d6
Doug Anderson <dianders@chromium.org>
2015-05-11 10:32:43 -0700

    Revert "mmc: core: Hold a wake lock accross delayed work + mmc rescan"
    This reverts commit 08054072af22f5220571e4408ca27ea8e96a33e1.
    
    In the CL (0805407 mmc: core: Hold a wake lock accross delayed work +
    mmc rescan) a wake lock was added to make sure that we could detect an
    SD card that was inserted right around the time the system was going
    into suspend (or maybe runtime suspend?).
    
    That CL really should have added a wake_lock per MMC host, not a
    single wake_lock for the whole MMC core subsystem.  Without adding a
    wakelock per host the different MMC users (eMMC, SD, SDIO-WiFi) will
    clobber each other's requests.
    
    The fact that there was _not_ one wake_lock per MMC host actually hid
    a bug.  mmc_rescan() has a few exit points that don't release the
    wake_lock, the most notable one being for NONREMOVABLE cards.
    
    This bug shows the race on rk3288-veyron systems.  On those systems we
    have 3 users of MMC (enumerated in this order):
    - mmc1: SD card   - removable
    - mmc0: eMMC      - non-removable
    - mmc2: SDIO-WIFI - non-removable
    
    If we add a enough delay before calling the wake_lock() for mmc2 then
    the mmc_rescan() for mmc1 will have time to fully run and have time to
    either call wake_lock_timeout() or wake_unlock() _before_ the mmc2
    request for wake_lock().  This shows that for mmc2 nobody was ever
    releasing the wake lock.  This is demonstrated in
    <http://crosreview.com/270185>.
    
    It wouldn't be terribly hard to move the wake_lock to be per host and
    to make sure we clear the wake_lock at all of the exit points of
    mmc_rescan(), but I'd rather someone who understood the motivation of
    the original CL do that (the original CL has no description of why it
    was needed nor links to any bugs).  For now, a revert seems safest
    without a good way for me to test the problem that the original CL was
    fixing.
    
    BUG=chrome-os-partner:39991
    TEST=suspend_stress_test
    
    Change-Id: I15081bf677adfe2aeff0302bc85636df3ebc0887
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/270186
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -31,7 +31,6 @@
 #include <linux/random.h>
 #include <linux/slab.h>
 #include <linux/of.h>
-#include <linux/wakelock.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/mmc.h>
@@ -65,7 +64,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(mmc_blk_rw_end);
 #define MMC_BKOPS_MAX_TIMEOUT	(4 * 60 * 1000) /* max time to wait in ms */
 
 static struct workqueue_struct *workqueue;
-static struct wake_lock mmc_delayed_work_wake_lock;
 static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
 
 /*
@@ -99,7 +97,6 @@ MODULE_PARM_DESC(
 static int mmc_schedule_delayed_work(struct delayed_work *work,
 				     unsigned long delay)
 {
-	wake_lock(&mmc_delayed_work_wake_lock);
 	return queue_delayed_work(workqueue, work, delay);
 }
 
@@ -2553,7 +2550,6 @@ void mmc_rescan(struct work_struct *work)
 	struct mmc_host *host =
 		container_of(work, struct mmc_host, detect.work);
 	int i;
-	bool extend_wakelock = false;
 
 	if (host->rescan_disable)
 		return;
@@ -2604,20 +2600,14 @@ void mmc_rescan(struct work_struct *work)
 
 	mmc_claim_host(host);
 	for (i = 0; i < ARRAY_SIZE(freqs); i++) {
-		if (!mmc_rescan_try_freq(host, max(freqs[i], host->f_min))) {
-			extend_wakelock = true;
+		if (!mmc_rescan_try_freq(host, max(freqs[i], host->f_min)))
 			break;
-		}
 		if (freqs[i] <= host->f_min)
 			break;
 	}
 	mmc_release_host(host);
 
  out:
-	if (extend_wakelock)
-		wake_lock_timeout(&mmc_delayed_work_wake_lock, HZ / 2);
-	else
-		wake_unlock(&mmc_delayed_work_wake_lock);
 	if (host->caps & MMC_CAP_NEEDS_POLL)
 		mmc_schedule_delayed_work(&host->detect, HZ);
 }
@@ -3001,9 +2991,6 @@ static int __init mmc_init(void)
 	if (!workqueue)
 		return -ENOMEM;
 
-	wake_lock_init(&mmc_delayed_work_wake_lock, WAKE_LOCK_SUSPEND,
-		       "mmc_delayed_work");
-
 	ret = mmc_register_bus();
 	if (ret)
 		goto destroy_workqueue;
@@ -3024,7 +3011,6 @@ unregister_bus:
 	mmc_unregister_bus();
 destroy_workqueue:
 	destroy_workqueue(workqueue);
-	wake_lock_destroy(&mmc_delayed_work_wake_lock);
 
 	return ret;
 }
@@ -3035,7 +3021,6 @@ static void __exit mmc_exit(void)
 	mmc_unregister_host_class();
 	mmc_unregister_bus();
 	destroy_workqueue(workqueue);
-	wake_lock_destroy(&mmc_delayed_work_wake_lock);
 }
 
 subsys_initcall(mmc_init);
