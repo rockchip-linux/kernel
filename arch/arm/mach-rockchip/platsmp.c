a343919eba7c243ec1a8ab2ae37f84fb64135d00
Caesar Wang <wxt@rock-chips.com>
2015-06-09 17:49:59 +0800

    FROMLIST: ARM: rockchip: fix the SMP code style
    Use the below scripts to check:
    scripts/checkpatch.pl -f --subject arch/arm/mach-rockchip/platsmp.c
    
    BUG=chrome-os-partner:39965
    TEST=use 'scripts/checkpatch.pl -f --subject' to check.
    
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    (am from https://patchwork.kernel.org/patch/6570651)
    
    Confilcts:
        read_cpuid_part --> read_cpuid_part_number
    
    Change-Id: Ifcb01ba0faff6906e9024a0f88db0d06cb7e4fc3
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/275418
    Reviewed-by: Douglas Anderson <dianders@chromium.org>

--- arch/arm/mach-rockchip/platsmp.c
+++ arch/arm/mach-rockchip/platsmp.c
@@ -100,7 +100,7 @@ static int pmu_set_power_domain(int pd, bool on)
 		ret = pmu_power_domain_is_on(pd);
 		if (ret < 0) {
 			pr_err("%s: could not read power domain state\n",
-				 __func__);
+			       __func__);
 			return ret;
 		}
 	}
@@ -130,7 +130,7 @@ static int __cpuinit rockchip_boot_secondary(unsigned int cpu,
 
 	if (cpu >= ncores) {
 		pr_err("%s: cpu %d outside maximum number of cpus %d\n",
-							__func__, cpu, ncores);
+		       __func__, cpu, ncores);
 		return -ENXIO;
 	}
 
@@ -140,7 +140,8 @@ static int __cpuinit rockchip_boot_secondary(unsigned int cpu,
 		return ret;
 
 	if (read_cpuid_part_number() != ARM_CPU_PART_CORTEX_A9) {
-		/* We communicate with the bootrom to active the cpus other
+		/*
+		 * We communicate with the bootrom to active the cpus other
 		 * than cpu0, after a blob of initialize code, they will
 		 * stay at wfe state, once they are actived, they will check
 		 * the mailbox:
@@ -149,11 +150,11 @@ static int __cpuinit rockchip_boot_secondary(unsigned int cpu,
 		 * The cpu0 need to wait the other cpus other than cpu0 entering
 		 * the wfe state.The wait time is affected by many aspects.
 		 * (e.g: cpu frequency, bootrom frequency, sram frequency, ...)
-		 * */
+		 */
 		mdelay(1); /* ensure the cpus other than cpu0 to startup */
 
 		writel(virt_to_phys(rockchip_secondary_startup),
-			sram_base_addr + 8);
+		       sram_base_addr + 8);
 		writel(0xDEADBEAF, sram_base_addr + 4);
 		dsb_sev();
 	}
@@ -336,7 +337,7 @@ static int rockchip_cpu_kill(unsigned int cpu)
 static void rockchip_cpu_die(unsigned int cpu)
 {
 	v7_exit_coherency_flush(louis);
-	while(1)
+	while (1)
 		cpu_do_idle();
 }
 #endif
@@ -349,4 +350,5 @@ static struct smp_operations rockchip_smp_ops __initdata = {
 	.cpu_die		= rockchip_cpu_die,
 #endif
 };
+
 CPU_METHOD_OF_DECLARE(rk3066_smp, "rockchip,rk3066-smp", &rockchip_smp_ops);
