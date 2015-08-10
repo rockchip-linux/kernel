bd8ffd9bb116b3c1e9593af0dc2743fe23192c61
Douglas Anderson <dianders@chromium.org>
2015-08-10 16:44:29 -0700

    CHROMIUM: ARM: rockchip: Hack in a rk3288 panic notifier to sample PC
    This implements the old "print the DBGPCSR at panic time" on the
    rk3288.  This is pretty much just a copy-paste job from
    <https://chromium-review.googlesource.com/#/c/66196/> but changed for
    the rk3288 address.
    
    This is obviously terribly ugly code.  We'll have to decide whether to
    land this or try to find something that will make upstream happy.
    Note that there is very little rk3288-specific here except the clocks
    and the base address.  The debug register we're looking at is generic
    to all A17s and the whole thing worked the same on A15 too.  That all
    means that there is likely to be much bikeshedding about how exactly
    to do this.  ...and maybe this needs to fit into a larger module for
    accessing the debug registers?  Certainly upstream has a "coresight"
    device tree property.
    
    BUG=chrome-os-partner:42868
    TEST=echo HARDLOCKUP > /sys/kernel/debug/provoke-crash/DIRECT
    
    Change-Id: I3b163f61286df752d3a14003ff190170a58aaa7e
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/292319
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>

--- arch/arm/mach-rockchip/rockchip.c
+++ arch/arm/mach-rockchip/rockchip.c
@@ -16,6 +16,7 @@
  */
 
 #include <linux/kernel.h>
+#include <linux/clk.h>
 #include <linux/init.h>
 #include <linux/of_platform.h>
 #include <linux/irqchip.h>
@@ -26,12 +27,70 @@
 #include "core.h"
 #include "pm.h"
 
+static void __iomem *rockchip_cpu_debug[4];
+static struct clk *pclk_dbg;
+static struct clk *pclk_core_niu;
+
+#define RK3288_DEBUG_PA_CPU(x)		(0xffbb0000 + (x * 0x2000))
+#define CPU_DBGPCSR			0xa0
+
+int rockchip_panic_notify(struct notifier_block *nb, unsigned long event,
+			 void *p)
+{
+	unsigned long dbgpcsr;
+	int i;
+	void *pc = NULL;
+
+	clk_enable(pclk_dbg);
+	clk_enable(pclk_core_niu);
+
+	for_each_online_cpu(i) {
+		dbgpcsr = readl_relaxed(rockchip_cpu_debug[i] + CPU_DBGPCSR);
+
+		/* NOTE: no offset on A17; see DBGDEVID1.PCSROffset */
+		pc = (void *)(dbgpcsr & ~1);
+
+		pr_err("CPU%d PC: <%p> %pF\n", i, pc, pc);
+	}
+	return NOTIFY_OK;
+}
+
+struct notifier_block rockchip_panic_nb = {
+	.notifier_call = rockchip_panic_notify,
+	.priority = INT_MAX,
+};
+
+static void __init rockchip_panic_init(void)
+{
+	int i;
+
+	/* These two clocks appear to be needed to access regs */
+	pclk_dbg = clk_get(NULL, "pclk_dbg");
+	if (WARN_ON(IS_ERR(pclk_dbg)))
+		return;
+	clk_prepare(pclk_dbg);
+
+	pclk_core_niu = clk_get(NULL, "pclk_core_niu");
+	if (WARN_ON(IS_ERR(pclk_core_niu)))
+		return;
+	clk_prepare(pclk_core_niu);
+
+	for (i = 0; i < 4; i++)
+		rockchip_cpu_debug[i] = ioremap(RK3288_DEBUG_PA_CPU(i), SZ_4K);
+
+	atomic_notifier_chain_register(&panic_notifier_list,
+				       &rockchip_panic_nb);
+}
+
 static void __init rockchip_dt_init(void)
 {
 	l2x0_of_init(0, ~0UL);
 	rockchip_suspend_init();
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
 	platform_device_register_simple("cpufreq-cpu0", 0, NULL, 0);
+
+	/* HACKY (and rk3288-specific) panic notifier */
+	rockchip_panic_init();
 }
 
 static void __init rockchip_memory_init(void)
