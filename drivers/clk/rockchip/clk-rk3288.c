ad66fbefff62acb2a792fcc2de1e7b352f844bc4
Douglas Anderson <dianders@chromium.org>
2015-08-26 13:48:55 -0700

    CHROMIUM: clk: rockchip: Add 243.428571 MHz for 121.75 MHz HDMI
    We'll add a rate useful for roughly making 121.75 MHz for HDMI.  We'll
    aim for low jitter.
    
    BUG=chrome-os-partner:44569
    TEST=Monitor works; check jitter
    
    Change-Id: I5f77238ed9265964a3a533efeca008067741c78b
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/295704
    Tested-by: Yakir Yang <ykk@rock-chips.com>
    Reviewed-by: Yakir Yang <ykk@rock-chips.com>
    (cherry picked from commit 6b3a1cadbbb3d57016cd87b8afc7ed95c4a0c922)
    Reviewed-on: https://chromium-review.googlesource.com/299526

--- drivers/clk/rockchip/clk-rk3288.c
+++ drivers/clk/rockchip/clk-rk3288.c
@@ -102,6 +102,7 @@ struct rockchip_pll_rate_table rk3288_pll_rates[] = {
 	RK3066_PLL_RATE( 273600000, 1, 114, 10),       /* Fvco = 2.736 GHz */
 	RK3066_PLL_RATE_NB( 270000000, 1, 180, 16, 1), /* Fvco = 4.320 GHz */
 	RK3066_PLL_RATE( 252000000, 1, 84, 8),
+	RK3066_PLL_RATE_NB( 243428571, 1, 142, 14, 1), /* Fvco = 3.408 GHz */
 	RK3066_PLL_RATE( 238000000, 1, 119, 12),       /* Fvco = 2.856 GHz */
 	RK3066_PLL_RATE( 216000000, 1, 72, 8),
 	RK3066_PLL_RATE( 160000000, 1, 80, 12),
