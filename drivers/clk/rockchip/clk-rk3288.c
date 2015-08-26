e7ed9f057c2128f5f14608b822a09d5ec58dcf9a
Douglas Anderson <dianders@chromium.org>
2015-08-26 13:48:55 -0700

    CHROMIUM: clk: rockchip: Add 195.428571 MHz for 97.75 MHz HDMI
    We'll add a rate useful for roughly making 97.75 MHz for HDMI.  Fvco
    is a little high-ish but not super high at 2.736 GHz.
    
    BUG=chrome-os-partner:44569
    TEST=Monitor works
    
    Change-Id: Ice2df50fd4736e3a1621a7128fbbb50e2db50948
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/295706
    Tested-by: Yakir Yang <ykk@rock-chips.com>
    Reviewed-by: Yakir Yang <ykk@rock-chips.com>
    (cherry picked from commit 367e128c3e677cc6c4d2c84cfb525e0ea6c921f1)
    Reviewed-on: https://chromium-review.googlesource.com/299553

--- drivers/clk/rockchip/clk-rk3288.c
+++ drivers/clk/rockchip/clk-rk3288.c
@@ -105,6 +105,7 @@ struct rockchip_pll_rate_table rk3288_pll_rates[] = {
 	RK3066_PLL_RATE_NB( 243428571, 1, 142, 14, 1), /* Fvco = 3.408 GHz */
 	RK3066_PLL_RATE( 238000000, 1, 119, 12),       /* Fvco = 2.856 GHz */
 	RK3066_PLL_RATE( 216000000, 1, 72, 8),
+	RK3066_PLL_RATE( 195428571, 1, 114, 14),       /* Fvco = 2.736 GHz */
 	RK3066_PLL_RATE( 160000000, 1, 80, 12),
 	RK3066_PLL_RATE( 126000000, 1, 84, 16),
 	{ /* sentinel */ },
