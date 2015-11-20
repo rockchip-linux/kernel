9a1d8b5f288013eb0df8beafeaefb904dfd1377a
Ruchi Kandoi <kandoiruchi@google.com>
2015-11-19 16:07:19 -0800

    trace: cpufreq: Add tracing for min/max cpufreq
    Change-Id: I73f6ec437c1f805437d9376abb6510d1364b07ec
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>

--- include/trace/events/power.h
+++ include/trace/events/power.h
@@ -48,6 +48,31 @@ DEFINE_EVENT(cpu, cpu_frequency,
 	TP_ARGS(frequency, cpu_id)
 );
 
+TRACE_EVENT(cpu_frequency_limits,
+
+	TP_PROTO(unsigned int max_freq, unsigned int min_freq,
+		unsigned int cpu_id),
+
+	TP_ARGS(max_freq, min_freq, cpu_id),
+
+	TP_STRUCT__entry(
+		__field(	u32,		min_freq	)
+		__field(	u32,		max_freq	)
+		__field(	u32,		cpu_id		)
+	),
+
+	TP_fast_assign(
+		__entry->min_freq = min_freq;
+		__entry->max_freq = min_freq;
+		__entry->cpu_id = cpu_id;
+	),
+
+	TP_printk("min=%lu max=%lu cpu_id=%lu",
+		  (unsigned long)__entry->min_freq,
+		  (unsigned long)__entry->max_freq,
+		  (unsigned long)__entry->cpu_id)
+);
+
 TRACE_EVENT(machine_suspend,
 
 	TP_PROTO(unsigned int state),
