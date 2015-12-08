rockchip-linux/kernel

commit 36fb8884486f6813b4810484d34d6eafa049f7cc

rk_sdmmc: Adjust the reset process for the MMC card in care of timeout
Change-Id: Iae86524eb95dcd261fa98a76f3ba291d136349f3
Signed-off-by: xiaoyao <xiaoyao@rock-chips.com>

drivers/mmc/host/rk_sdmmc.c
@@ -1989,20 +1989,13 @@ static void dw_mci_post_tmo(struct mmc_host *mmc)
 	u32 i, regs, cmd_flags;
 	u32 sdio_int;
 	unsigned long timeout = 0;
-	bool ret_timeout = true, is_retry = false;
+	bool ret_timeout = true;
 	u32 opcode;
 
-	if (host->cur_slot->mrq->data)
-		dw_mci_stop_dma(host);
-
 	opcode = host->mrq->cmd->opcode;
-	host->cur_slot->mrq = NULL;
-	host->mrq = NULL;
-	host->state = STATE_IDLE;
-	host->data = NULL;
-
+	/* In tune mode, the timeout is normal */
 	if ((opcode == MMC_SEND_TUNING_BLOCK_HS200) ||
-	    (opcode == MMC_SEND_TUNING_BLOCK))
+		(opcode == MMC_SEND_TUNING_BLOCK))
 		return;
 
 	printk("[%s] -- Timeout recovery procedure start --\n",
@@ -2010,6 +2003,15 @@ static void dw_mci_post_tmo(struct mmc_host *mmc)
 
 	/* unmask irq */
 	mci_writel(host, INTMASK, 0x0);
+
+	/* send stop dma */
+	if (host->cur_slot->mrq->data)
+		dw_mci_stop_dma(host);
+
+	host->cur_slot->mrq = NULL;
+	host->mrq = NULL;
+	host->state = STATE_IDLE;
+	host->data = NULL;
 
 retry_stop:
 	/* send stop cmd */
@@ -2032,15 +2034,19 @@ retry_stop:
 	}
 
 	if (false == ret_timeout) {
-		MMC_DBG_ERR_FUNC(host->mmc, "stop recovery failed![%s]",
+		MMC_DBG_ERR_FUNC(host->mmc, "stop cmd recovery failed![%s]",
 				 mmc_hostname(host->mmc));
 		/* pd_peri mmc AHB bus software reset request */
 		rockchip_mmc_reset_controller(host->reset);
-	} else {
-		if (!dw_mci_ctrl_all_reset(host))
-			return;
-		if (is_retry == true)
-			goto recovery_end;
+		goto retry_stop;
+	}
+
+	if (!dw_mci_ctrl_all_reset(host)) {
+		MMC_DBG_ERR_FUNC(host->mmc, "all reset recovery failed![%s]",
+				 mmc_hostname(host->mmc));
+		/* pd_peri mmc AHB bus software reset request */
+		rockchip_mmc_reset_controller(host->reset);
+		goto retry_stop;
 	}
 
 #ifdef CONFIG_MMC_DW_IDMAC
@@ -2085,13 +2091,7 @@ retry_stop:
 		}
 	}
 	mci_writel(host, RINTSTS, 0xFFFFFFFF);
-	if (ret_timeout == false) {
-		ret_timeout = true;
-		is_retry = true;
-		goto retry_stop;
-	}
 
-recovery_end:
 	printk("[%s] -- Timeout recovery procedure finished --\n",
 		mmc_hostname(host->mmc));
 }

commit b21d3fb487917b10b2eed554aaf593157f8c5a88

mmc: rk_sdmmc: fix reset procedure in hs200 timeout
the interrupt is required to close and some variables
are cleared, in hs200 timeout.

the following during boot:
[    3.023316]  dwmmc_rockchip ff0f0000.rksdmmc: req failed (CMD21): error = -110, timeout = 100ms
[    3.023441]  ------------[ cut here ]------------
[    3.023555]  WARNING: at drivers/mmc/host/rk_sdmmc.c:1280 dw_mci_request+0x3c/0x180()
[    3.023639]  Modules linked in:
[    3.023750]  CPU: 1 PID: 64 Comm: kworker/u16:1 Tainted: G        W    3.10.0 #19
[    3.023852]  Workqueue: kmmcd mmc_rescan
[    3.023938]  Call trace:
[    3.024037]  [<ffffffc000088468>] dump_backtrace+0x0/0x10c
[    3.024137]  [<ffffffc000088584>] show_stack+0x10/0x1c
[    3.024233]  [<ffffffc0008d4774>] dump_stack+0x1c/0x28
[    3.024330]  [<ffffffc0000a69d0>] warn_slowpath_common+0x74/0x9c
[    3.024427]  [<ffffffc0000a6abc>] warn_slowpath_null+0x14/0x20
[    3.024528]  [<ffffffc00064fa6c>] dw_mci_request+0x38/0x180
[    3.024625]  [<ffffffc00063b13c>] __mmc_start_request+0x40/0x50

Fixes: 36fb88844 ("Adjust the reset process for the MMC ...")
Change-Id: I9dc16d8ad981a60ada576b20ac2568b8674dd383
Signed-off-by: xiaoyao <xiaoyao@rock-chips.com>

drivers/mmc/host/rk_sdmmc.c
@@ -1910,29 +1910,32 @@ static void dw_mci_post_tmo(struct mmc_host *mmc)
 	u32 sdio_int;
 	unsigned long timeout = 0;
 	bool ret_timeout = true;
-	u32 opcode;
-
-	opcode = host->mrq->cmd->opcode;
-	/* In tune mode, the timeout is normal */
-	if ((opcode == MMC_SEND_TUNING_BLOCK_HS200) ||
-		(opcode == MMC_SEND_TUNING_BLOCK))
-		return;
-
-	dev_info(host->dev,
-		 "[%s] -- Timeout recovery procedure start --\n",
-		 mmc_hostname(host->mmc));
+	u32 opcode, int_val;
 
 	/* unmask irq */
+	int_val = mci_readl(host, INTMASK);
 	mci_writel(host, INTMASK, 0x0);
 
 	/* send stop dma */
 	if (host->cur_slot->mrq->data)
 		dw_mci_stop_dma(host);
 
+	opcode = host->mrq->cmd->opcode;
 	host->cur_slot->mrq = NULL;
 	host->mrq = NULL;
 	host->state = STATE_IDLE;
 	host->data = NULL;
+
+	/* In tune mode, the timeout is normal */
+	if ((opcode == MMC_SEND_TUNING_BLOCK_HS200) ||
+		(opcode == MMC_SEND_TUNING_BLOCK)) {
+		mci_writel(host, INTMASK, int_val);
+		return;
+	}
+
+	dev_info(host->dev,
+		 "[%s] -- Timeout recovery procedure start --\n",
+		 mmc_hostname(host->mmc));
 
 retry_stop:
 	/* send stop cmd */