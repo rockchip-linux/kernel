a005677fb6b8eaf8965675d3e0427964ff85f5db
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2016-01-13 17:42:54 +0100

    kernel/time/timer: SMP=n fix
    Reported-by: Grygorii Strashko <grygorii.strashko@ti.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -1118,7 +1118,7 @@ int try_to_del_timer_sync(struct timer_list *timer)
 }
 EXPORT_SYMBOL(try_to_del_timer_sync);
 
-#ifdef CONFIG_SMP
+#if defined(CONFIG_SMP) || defined(CONFIG_PREEMPT_RT_FULL)
 /**
  * del_timer_sync - deactivate a timer and wait for the handler to finish.
  * @timer: the timer to be deactivated
