46ee7adeedf4cfdb4d5f8b0eaa1bf0a4c32be008
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2016-01-21 16:05:35 +0100

    x86: lazy preempt: use the need resched bits in EXIT_TO_USERMODE_LOOP_FLAGS
    missed during v4.1..v4.4 port.
    
    Reported-And-Patch-By: Mike Galbraith <umgwanakikbuti@gmail.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- arch/x86/entry/common.c
+++ arch/x86/entry/common.c
@@ -220,7 +220,7 @@ long syscall_trace_enter(struct pt_regs *regs)
 
 #define EXIT_TO_USERMODE_LOOP_FLAGS				\
 	(_TIF_SIGPENDING | _TIF_NOTIFY_RESUME | _TIF_UPROBE |	\
-	 _TIF_NEED_RESCHED | _TIF_USER_RETURN_NOTIFY)
+	 _TIF_NEED_RESCHED_MASK | _TIF_USER_RETURN_NOTIFY)
 
 static void exit_to_usermode_loop(struct pt_regs *regs, u32 cached_flags)
 {
