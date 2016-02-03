12f0688e47940eecbbd324d0e0c5780c7c77df8c
Dan Carpenter <dan.carpenter@oracle.com>
2016-02-03 13:34:00 -0200

    UPSTREAM: [media] xc2028: unlock on error in xc2028_set_config()
    (cherry picked from commit 210bd104c6acd31c3c6b8b075b3f12d4a9f6b60d)
    
    We have to unlock before returning -ENOMEM.
    
    Fixes: 8dfbcc4351a0 ('[media] xc2028: avoid use after free')
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>
    Change-Id: I7b6ba9fde5c6e29467e6de23d398af2fe56e2547
    Bug: 30946097

--- drivers/media/tuners/tuner-xc2028.c
+++ drivers/media/tuners/tuner-xc2028.c
@@ -1383,8 +1383,10 @@ static int xc2028_set_config(struct dvb_frontend *fe, void *priv_cfg)
 	memcpy(&priv->ctrl, p, sizeof(priv->ctrl));
 	if (p->fname) {
 		priv->ctrl.fname = kstrdup(p->fname, GFP_KERNEL);
-		if (priv->ctrl.fname == NULL)
-			return -ENOMEM;
+		if (priv->ctrl.fname == NULL) {
+			rc = -ENOMEM;
+			goto unlock;
+		}
 	}
 
 	/*
@@ -1416,6 +1418,7 @@ static int xc2028_set_config(struct dvb_frontend *fe, void *priv_cfg)
 		} else
 			priv->state = XC2028_WAITING_FIRMWARE;
 	}
+unlock:
 	mutex_unlock(&priv->lock);
 
 	return rc;
