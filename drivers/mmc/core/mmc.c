15e6d64a828185e2d5673db26b43dda79dd26006
xiaoyao <xiaoyao@rock-chips.com>
2016-04-25 10:52:36 +0800

    mmc: core: support RPMB for rockchip platform
    Change-Id: I7b952594f3283c0fe76038c9693c14a3f99b28d1
    Signed-off-by: xiaoyao <xiaoyao@rock-chips.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -501,14 +501,12 @@ static int mmc_read_ext_csd(struct mmc_card *card, u8 *ext_csd)
 		 * RPMB regions are defined in multiples of 128K.
 		 */
 		card->ext_csd.raw_rpmb_size_mult = ext_csd[EXT_CSD_RPMB_MULT];
-		#if 0 //noted by xbw,2014-03-11
 		if (ext_csd[EXT_CSD_RPMB_MULT] && mmc_host_cmd23(card->host)) {
 			mmc_part_add(card, ext_csd[EXT_CSD_RPMB_MULT] << 17,
 				EXT_CSD_PART_CONFIG_ACC_RPMB,
 				"rpmb", 0, false,
 				MMC_BLK_DATA_AREA_RPMB);
 		}
-		#endif
 	}
 
 	card->ext_csd.raw_erased_mem_count = ext_csd[EXT_CSD_ERASED_MEM_CONT];
