579e079b38045e84939aba80fb839c1ea11d7420
David Wu <david.wu@rock-chips.com>
2016-06-03 16:55:57 +0800

    i2c: rk3x: add i2c support for rk1108
    Change-Id: Ifccb4bd8a9a4d59fe0d736b9bf4911724890979c
    Signed-off-by: David Wu <david.wu@rock-chips.com>

--- drivers/i2c/busses/i2c-rk3x.c
+++ drivers/i2c/busses/i2c-rk3x.c
@@ -1121,6 +1121,11 @@ static const struct i2c_algorithm rk3x_i2c_algorithm = {
 	.functionality		= rk3x_i2c_func,
 };
 
+static const struct rk3x_i2c_soc_data rk1108_soc_data = {
+	.grf_offset = -1,
+	.calc_timings = rk3x_i2c_v1_calc_timings,
+};
+
 static const struct rk3x_i2c_soc_data rk3066_soc_data = {
 	.grf_offset = 0x154,
 	.calc_timings = rk3x_i2c_v0_calc_timings,
@@ -1147,6 +1152,10 @@ static const struct rk3x_i2c_soc_data rk3399_soc_data = {
 };
 
 static const struct of_device_id rk3x_i2c_match[] = {
+	{
+		.compatible = "rockchip,rk1108-i2c",
+		.data = (void *)&rk1108_soc_data
+	},
 	{
 		.compatible = "rockchip,rk3066-i2c",
 		.data = (void *)&rk3066_soc_data
