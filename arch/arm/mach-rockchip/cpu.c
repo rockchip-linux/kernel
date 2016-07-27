149a83727e866a9cc1f25160b1dbfc03a6957707
Huang zhibao <hzb@rock-chips.com>
2016-07-27 08:55:24 +0800

    arm: rockchip: cpu: add show rk3288w soc type
    Change-Id: Ic7c85ae02fcb92ec23fce6353d888a175c0df742
    Signed-off-by: Huang zhibao <hzb@rock-chips.com>

--- arch/arm/mach-rockchip/cpu.c
+++ arch/arm/mach-rockchip/cpu.c
@@ -43,8 +43,10 @@ static ssize_t soc_show(struct device *dev, struct device_attribute *attr, char 
 {
 	const char *soc;
 
-	if (soc_is_rk3288() || soc_is_rk3288w())
+	if (soc_is_rk3288())
 		soc = "rk3288";
+	else if (soc_is_rk3288w())
+		soc = "rk3288w";
 	else if (soc_is_rk3190())
 		soc = "rk3190";
 	else if (soc_is_rk3188plus())
