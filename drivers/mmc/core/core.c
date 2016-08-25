6ff842a94ae04c7df0cd39b1e63567960cb6b08c
Mohan Srinivasan <srmohan@google.com>
2016-08-25 14:42:36 -0700

    ANDROID: MMC: Fix a 32 bit build breakage.
    The MMC IO latency histogram support broke 32 bit builds, because
    it didn't use the arch lib functions to do 64 bit division. Fixed
    by using div64_u64(). Tested on both arm 64 bit and 32 bit devices.
    
    Change-Id: I8e3e700021f96e2f5f94e5e3b068ec2d5afefa27
    Signed-off-by: Mohan Srinivasan <srmohan@google.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -36,6 +36,8 @@
 #include <linux/mmc/mmc.h>
 #include <linux/mmc/sd.h>
 
+#include <linux/math64.h>
+
 #include "core.h"
 #include "bus.h"
 #include "host.h"
@@ -2957,19 +2959,19 @@ latency_hist_show(struct device *dev, struct device_attribute *attr, char *buf)
 	int i;
 	struct mmc_host *host = cls_dev_to_mmc_host(dev);
 	int bytes_written = 0;
-	int pct, num_elem;
-	u_int64_t elem;
+	u_int64_t num_elem, elem;
+	int pct;
 
 	num_elem = host->latency_reads_elems;
 	if (num_elem > 0) {
 		bytes_written += scnprintf(buf + bytes_written,
-					   PAGE_SIZE - bytes_written,
-					   "IO svc_time Read Latency Histogram :\n");
+			   PAGE_SIZE - bytes_written,
+			   "IO svc_time Read Latency Histogram :\n");
 		for (i = 0;
 		     i < ARRAY_SIZE(latency_x_axis_us);
 		     i++) {
 			elem = host->latency_y_axis_read[i];
-			pct = (elem * 100) / num_elem;
+			pct = div64_u64(elem * 100, num_elem);
 			bytes_written += scnprintf(buf + bytes_written,
 						   PAGE_SIZE - bytes_written,
 						   "\t< %5lluus%15llu%15d%%\n",
@@ -2978,7 +2980,7 @@ latency_hist_show(struct device *dev, struct device_attribute *attr, char *buf)
 		}
 		/* Last element in y-axis table is overflow */
 		elem = host->latency_y_axis_read[i];
-		pct = (elem * 100) / num_elem;
+		pct = div64_u64(elem * 100, num_elem);
 		bytes_written += scnprintf(buf + bytes_written,
 					   PAGE_SIZE - bytes_written,
 					   "\t> %5dms%15llu%15d%%\n", 10,
@@ -2993,7 +2995,7 @@ latency_hist_show(struct device *dev, struct device_attribute *attr, char *buf)
 		     i < ARRAY_SIZE(latency_x_axis_us);
 		     i++) {
 			elem = host->latency_y_axis_write[i];
-			pct = (elem * 100) / num_elem;
+			pct = div64_u64(elem * 100, num_elem);
 			bytes_written += scnprintf(buf + bytes_written,
 						   PAGE_SIZE - bytes_written,
 						   "\t< %5lluus%15llu%15d%%\n",
@@ -3002,7 +3004,7 @@ latency_hist_show(struct device *dev, struct device_attribute *attr, char *buf)
 		}
 		/* Last element in y-axis table is overflow */
 		elem = host->latency_y_axis_write[i];
-		pct = (elem * 100) / num_elem;
+		pct = div64_u64(elem * 100, num_elem);
 		bytes_written += scnprintf(buf + bytes_written,
 					   PAGE_SIZE - bytes_written,
 					   "\t> %5dms%15llu%15d%%\n", 10,
