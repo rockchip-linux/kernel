eb8b53b8721b268121b2c902eff93e7588fcb6ea
Huang, Tao <huangtao@rock-chips.com>
2016-10-25 18:31:11 +0800

    drm/rockchip: remove support
    Rockchip DRM support is test only on 3.10, just remove it.
    
    Change-Id: Id059512538fab69d1ef10bcb031bbfb71ffc5efc
    Signed-off-by: Huang, Tao <huangtao@rock-chips.com>

--- include/linux/rk_fb.h
+++ include/linux/rk_fb.h
@@ -748,9 +748,6 @@ struct rk_lcdc_driver {
 	struct list_head pwrlist_head;
 	struct rk_lcdc_drv_ops *ops;
 	struct rk_fb_trsm_ops *trsm_ops;
-#ifdef CONFIG_DRM_ROCKCHIP
-	void (*irq_call_back)(struct rk_lcdc_driver *driver);
-#endif
 	struct overscan overscan;
 	struct rk_lcdc_bcsh bcsh;
 	int *hwc_lut;
