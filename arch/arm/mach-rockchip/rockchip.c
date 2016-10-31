1ec20817f5b546e2607d83db0bd030475926bcf3
Jacob Chen <jacob2.chen@rock-chips.com>
2016-10-31 23:52:13 +0800

    multiple-os: don't set rktimer in kernel
    uboot will use rktimer
    
    Change-Id: Icb9bdf6f04916198296433a67332f28f9b43674e
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- arch/arm/mach-rockchip/rockchip.c
+++ arch/arm/mach-rockchip/rockchip.c
@@ -45,16 +45,16 @@ static void __init rockchip_timer_init(void)
 		 * So make sure it is running during early boot.
 		 */
 		reg_base = ioremap(RK3288_TIMER6_7_PHYS, SZ_16K);
-		if (reg_base) {
-			writel(0, reg_base + 0x30);
-			writel(0xffffffff, reg_base + 0x20);
-			writel(0xffffffff, reg_base + 0x24);
-			writel(1, reg_base + 0x30);
-			dsb();
-			iounmap(reg_base);
-		} else {
-			pr_err("rockchip: could not map timer7 registers\n");
-		}
+		// if (reg_base) {
+		// 	writel(0, reg_base + 0x30);
+		// 	writel(0xffffffff, reg_base + 0x20);
+		// 	writel(0xffffffff, reg_base + 0x24);
+		// 	writel(1, reg_base + 0x30);
+		// 	dsb();
+		// 	iounmap(reg_base);
+		// } else {
+		// 	pr_err("rockchip: could not map timer7 registers\n");
+		// }
 
 		/*
 		 * Disable auto jtag/sdmmc switching that causes issues
