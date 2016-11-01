0a2e1de18eafb274127975d37346215b5b779201
Meng Dongyang <daniel.meng@rock-chips.com>
2016-11-01 19:12:44 +0800

    usb: dwc2: add dwc2 support for rk322xh
    Add rk322xh u2phy driver for special clock and reset signal. With
    the interface of this driver, the dwc2 controller driver can reset
    and enable or disable the controller or phy.
    
    Change-Id: I370a09d66d405ab83425f418388bbda411fee9b1
    Signed-off-by: Meng Dongyang <daniel.meng@rock-chips.com>

--- drivers/usb/dwc_otg_310/usbdev_rk.h
+++ drivers/usb/dwc_otg_310/usbdev_rk.h
@@ -84,6 +84,8 @@ extern struct dwc_otg_platform_data usb20otg_pdata_rk3368;
 extern struct dwc_otg_platform_data usb20otg_pdata_rk322x;
 /* rk1108 platform data */
 extern struct dwc_otg_platform_data usb20otg_pdata_rk1108;
+/* rk322xh platform data */
+extern struct dwc_otg_platform_data usb20otg_pdata_rk322xh;
 
 struct dwc_otg_platform_data {
 	void *privdata;
