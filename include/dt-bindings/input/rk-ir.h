42dfe87cb11bc03dd950cf6725e0ba9282d73738
Zhangbin Tong <zebulun.tong@rock-chips.com>
2016-11-16 16:17:44 +0800

    arm64: dts: rockchip: rk322xh-evb: add stb ir keymap
    Change-Id: I1c51aee1b4384938e534b189e65bf0b89e446940
    Signed-off-by: Zhangbin Tong <zebulun.tong@rock-chips.com>

---
+++ include/dt-bindings/input/rk-ir.h
@@ -0,0 +1,42 @@
+/*
+ * RK IR Keys definition
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARRKCULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef _DT_BINDINGS_RK_IR_H
+#define _DT_BINDINGS_RK_IR_H
+
+/* STB KEYS */
+#define KEY_CHANNEL_UP		249
+#define KEY_CHANNEL_DN		250
+#define KEY_HOME_PAGE		251
+#define KEY_CH_CUT_BACK		252
+#define KEY_DIRECT_SEEDING	253
+#define KEY_REVIEW		254
+#define KEY_ON_DEMAND		255
+#define KEY_INFO1		256
+#define KEY_SOUND1		257
+#define KEY_X1			258
+#define KEY_X2			259
+#define KEY_LOCAL		260
+#define KEY_APPLICATION		261
+#define KEY_POS			262
+#define KEY_GO_BEGINNING	263
+#define KEY_INTERX		264
+#define KEY_FAVORITE		265
+#define KEY_CHANNEL_POS		266
+#define KEY_EVENT		267
+#define KEY_COMM		268
+#define KEY_LAUNCH		269
+#define KEY_TRACK		270
+#define KEY_PORTAL		271
+
+#endif
