6655386164db87fa6a22c068c218cd3a1047614c
Zheng Yang <zhengyang@rock-chips.com>
2016-12-05 19:22:04 +0800

    video: rockchip: hdmi: disable 322xh SUPPORT_TMDS_600M feature
    Change-Id: I81a50ab34b69c3e37ed3893b85927aec55a6c529
    Signed-off-by: Zheng Yang <zhengyang@rock-chips.com>

--- drivers/video/rockchip/hdmi/rockchip-hdmiv2/rockchip_hdmiv2.c
+++ drivers/video/rockchip/hdmi/rockchip-hdmiv2/rockchip_hdmiv2.c
@@ -649,8 +649,7 @@ static int rockchip_hdmiv2_probe(struct platform_device *pdev)
 				SUPPORT_1080I |
 				SUPPORT_480I_576I |
 				SUPPORT_YUV420 |
-				SUPPORT_DEEP_10BIT |
-				SUPPORT_TMDS_600M;
+				SUPPORT_DEEP_10BIT;
 	} else {
 		ret = -ENXIO;
 		goto failed1;
