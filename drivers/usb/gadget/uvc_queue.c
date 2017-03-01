1d0f4efc8bf57b352eaaec74b03a75a266a5c421
William Wu <wulf@rock-chips.com>
2017-03-01 22:44:12 +0800

    usb: gadget: uvc: set timestamp_type
    Use the monotonic clock and set the timestamp_type that vb2
    expects. This can avoid warnings about UNDEFINED timestamp
    type in drivers/media/v4l2-core/videobuf2-core.c.
    
    Change-Id: Icd677a0f2e7dffcc6bd666893d6e6deb6242aa34
    Signed-off-by: William Wu <wulf@rock-chips.com>

--- drivers/usb/gadget/uvc_queue.c
+++ drivers/usb/gadget/uvc_queue.c
@@ -120,6 +120,7 @@ static int uvc_queue_init(struct uvc_video_queue *queue,
 	queue->queue.buf_struct_size = sizeof(struct uvc_buffer);
 	queue->queue.ops = &uvc_queue_qops;
 	queue->queue.mem_ops = &vb2_vmalloc_memops;
+	queue->queue.timestamp_type = V4L2_BUF_FLAG_TIMESTAMP_MONOTONIC;
 	ret = vb2_queue_init(&queue->queue);
 	if (ret)
 		return ret;
