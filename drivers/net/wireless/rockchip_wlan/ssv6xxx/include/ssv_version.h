963c3a201730a825b0641d40404b2c25e5655fd7
Xu Xuehui <xxh@rock-chips.com>
2017-03-03 11:59:30 +0800

    net: wireless: rockchip_wlan: support ssv6051p sdio wifi
    wifi driver version: 6051.Q0.1009.15.400403
    
    Change-Id: I70b3e45ed27d2e55e3e0827948109e4e81d5dbc9
    Signed-off-by: Xu Xuehui <xxh@rock-chips.com>

---
+++ drivers/net/wireless/rockchip_wlan/ssv6xxx/include/ssv_version.h
@@ -0,0 +1,13 @@
+#ifndef _SSV_VERSION_H_
+#define _SSV_VERSION_H_
+
+static u32 ssv_root_version = 13696;
+
+#define SSV_ROOT_URl "http://192.168.15.30/svn/software/project/release/android/box/rk3x28/6051.Q0.1009.15.400403/ssv6xxx"
+#define COMPILERHOST "software-pc"
+#define COMPILERDATE "11-08-2016-17:36:11"
+#define COMPILEROS "linux"
+#define COMPILEROSARCH "x86_64-linux-gnu-thread-multi"
+
+#endif
+
