11a41290b97eb4c99cd2d9ff0fee48e1df0dcdf7
William Wu <william.wu@rock-chips.com>
2017-05-02 14:14:31 +0800

    Revert "usb: gadget: uvc: increase isoc streaming maxpacket"
    This reverts commit 1ac28618ff56fd1cb4360b1537489b56145974da.
    
    We find that mjpg/h264 360p will transfer fail if the maxpacket
    is set to 3072 bytes. We haven't found the root cause yet, so
    just revert this commit until we fix the issue.
    
    Change-Id: Ia047815f647e1ae9da76857f265bbb9381e5db5d
    Signed-off-by: William Wu <william.wu@rock-chips.com>

--- drivers/usb/gadget/f_uvc.c
+++ drivers/usb/gadget/f_uvc.c
@@ -37,7 +37,7 @@ static unsigned int streaming_interval = 1;
 module_param(streaming_interval, uint, S_IRUGO|S_IWUSR);
 MODULE_PARM_DESC(streaming_interval, "1 - 16");
 
-static unsigned int streaming_maxpacket = 3072;
+static unsigned int streaming_maxpacket = 1024;
 module_param(streaming_maxpacket, uint, S_IRUGO|S_IWUSR);
 MODULE_PARM_DESC(streaming_maxpacket, "1 - 1023 (FS), 1 - 3072 (hs/ss)");
 
