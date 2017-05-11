9ba3871a1139523cb0da99fb814a49da6fb06a2a
chenjh <chenjh@rock-chips.com>
2017-05-11 11:52:49 +0800

    power: rk818-battery: dump charger update info
    Change-Id: Ia56ab4a0047a2bd1aa7639476c07ea964a7f354d
    Signed-off-by: chenjh <chenjh@rock-chips.com>

--- drivers/power/rk818_battery.c
+++ drivers/power/rk818_battery.c
@@ -1302,7 +1302,10 @@ static void rk818_term_mode_work(struct work_struct *work)
 static void rk818_bat_set_chrg_param(struct rk818_battery *di,
 				     enum charger_type charger_type)
 {
-	u8 buf;
+	u8 buf, usb_ctrl, chrg_ctrl1;
+	static const char *const charge_name[] = {"UNKNOWN", "NONE_USB_DC",
+						  "NONE_USB", "USB", "AC",
+						  "DC_DC", "NONE_DC"};
 
 	switch (charger_type) {
 	case USB_DC_TYPE_NONE_CHARGER:
@@ -1380,6 +1383,13 @@ static void rk818_bat_set_chrg_param(struct rk818_battery *di,
 		di->prop_val = POWER_SUPPLY_STATUS_DISCHARGING;
 		break;
 	}
+
+	usb_ctrl = rk818_bat_read(di, RK818_USB_CTRL_REG);
+	chrg_ctrl1 = rk818_bat_read(di, RK818_CHRG_CTRL_REG1);
+	BAT_INFO("set charger type: %s, current: input=%d, chrg=%d\n",
+		 charge_name[charger_type],
+		 CHRG_CUR_INPUT[usb_ctrl & 0x0f],
+		 CHRG_CUR_SEL[chrg_ctrl1 & 0x0f]);
 
 	if (di->dsoc == 100 && rk818_bat_chrg_online(di))
 		di->prop_val = POWER_SUPPLY_STATUS_FULL;
