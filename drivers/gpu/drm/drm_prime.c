4b0c2757bbebfd118d59d4fff95bfbb13fa111b4
Lucas Stach <l.stach@pengutronix.de>
2017-11-30 18:34:28 +0100

    BACKPORT: drm/prime: skip CPU sync in map/unmap dma_buf
    Dma-bufs should already be device coherent, as they are only pulled in the
    CPU domain via the begin/end cpu_access calls. As we cache the mapping set
    up by dma_map_sg a CPU sync at this point will not actually guarantee proper
    coherency on non-coherent architectures, so we can as well stop pretending.
    
    This is an important performance fix for architectures which need explicit
    cache synchronization and userspace doing lots of dma-buf imports.
    Improves Weston on Etnaviv performance 5x, where before this patch > 90%
    of Weston CPU time was spent synchronizing caches for buffers which are
    already device coherent.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20171130173428.8666-1-l.stach@pengutronix.de
    (cherry picked from commit ca0e68e21aae10220eff71a297e7d794425add77)
    
    Change-Id: Ifeb7adbe8aa2f6f6dbb6bbe0a467122317e3e379
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- drivers/gpu/drm/drm_prime.c
+++ drivers/gpu/drm/drm_prime.c
@@ -157,9 +157,12 @@ static void drm_gem_map_detach(struct dma_buf *dma_buf,
 
 	sgt = prime_attach->sgt;
 	if (sgt) {
-		if (prime_attach->dir != DMA_NONE)
-			dma_unmap_sg(attach->dev, sgt->sgl, sgt->nents,
-					prime_attach->dir);
+		if (prime_attach->dir != DMA_NONE) {
+			DEFINE_DMA_ATTRS(attrs);
+			dma_set_attr(DMA_ATTR_SKIP_CPU_SYNC, &attrs);
+			dma_unmap_sg_attrs(attach->dev, sgt->sgl, sgt->nents,
+					   prime_attach->dir, &attrs);
+		}
 		sg_free_table(sgt);
 	}
 
@@ -206,7 +209,10 @@ static struct sg_table *drm_gem_map_dma_buf(struct dma_buf_attachment *attach,
 	sgt = obj->dev->driver->gem_prime_get_sg_table(obj);
 
 	if (!IS_ERR(sgt)) {
-		if (!dma_map_sg(attach->dev, sgt->sgl, sgt->nents, dir)) {
+		DEFINE_DMA_ATTRS(attrs);
+		dma_set_attr(DMA_ATTR_SKIP_CPU_SYNC, &attrs);
+		if (!dma_map_sg_attrs(attach->dev, sgt->sgl, sgt->nents, dir,
+				      &attrs)) {
 			sg_free_table(sgt);
 			kfree(sgt);
 			sgt = ERR_PTR(-ENOMEM);
