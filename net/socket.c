2bc86aa8cfbd83f0c0ef3b8da46bf5ef017004b3
huweiguo <hwg@rock-chips.com>
2017-12-26 09:55:34 +0800

    net: socket: add SIOCKILLADDR
    Change-Id: I777dbab71eedd95e31092f56b102556769f14bd9
    Signed-off-by: huweiguo <hwg@rock-chips.com>

--- net/socket.c
+++ net/socket.c
@@ -3316,6 +3316,7 @@ static int compat_sock_ioctl_trans(struct file *file, struct socket *sock,
 	case SIOCGMIIPHY:
 	case SIOCGMIIREG:
 	case SIOCSMIIREG:
+	case SIOCKILLADDR:
 		return dev_ifsioc(net, sock, cmd, argp);
 
 	case SIOCSARP:
