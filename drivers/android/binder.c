0642c259bda8c5d19b888ca91d640c312886d71d
宋金时 <songjinshi@xiaomi.com>
2018-05-10 02:05:03 +0000

    UPSTREAM: ANDROID: binder: correct the cmd print for BINDER_WORK_RETURN_ERROR
    When to execute binder_stat_br the e->cmd has been modifying as BR_OK
    instead of the original return error cmd, in fact we want to know the
    original return error, such as BR_DEAD_REPLY or BR_FAILED_REPLY, etc.
    instead of always BR_OK, in order to avoid the value of the e->cmd is
    always BR_OK, so we need assign the value of the e->cmd to cmd before
    e->cmd = BR_OK.
    
    Signed-off-by: songjinshi <songjinshi@xiaomi.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 838d5565669aa5bb7deb605684a5970d51d5eaf6)
    
    Change-Id: I425b32c5419a491c6b9ceee7c00dde6513e0421d

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -4031,6 +4031,7 @@ retry:
 			binder_inner_proc_unlock(proc);
 			if (put_user(e->cmd, (uint32_t __user *)ptr))
 				return -EFAULT;
+			cmd = e->cmd;
 			e->cmd = BR_OK;
 			ptr += sizeof(uint32_t);
 
