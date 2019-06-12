f3982dc22be659e3a3ae4f169ec45f34333d5c63
Bin Yang <yangbin@rock-chips.com>
2019-06-12 10:33:01 +0800

    usb: cdc_acm: Ignore Rockchip NPU ACM device
    Rockchip NPU usb function is configured to an ACM device
    for RK3399pro-N4. The ACM device needs to use a special
    driver, so we need to ignore cdc-acm universal driver.
    
    Change-Id: I725ba3d415f9556588c25f34abe31138a051e303
    Signed-off-by: Bin Yang <yangbin@rock-chips.com>

--- drivers/usb/class/cdc-acm.c
+++ drivers/usb/class/cdc-acm.c
@@ -1892,6 +1892,11 @@ static const struct usb_device_id acm_ids[] = {
 	.driver_info = SEND_ZERO_PACKET,
 	},
 
+	/* Rockchip NPU ACM device */
+	{ USB_DEVICE(0x2207, 0x1005),
+	.driver_info = IGNORE_DEVICE
+	},
+
 	/* control interfaces without any protocol set */
 	{ USB_INTERFACE_INFO(USB_CLASS_COMM, USB_CDC_SUBCLASS_ACM,
 		USB_CDC_PROTO_NONE) },
