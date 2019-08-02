17ea71a50d7eee9d7ac1efa4a36de0717b68e75a
djw <service@t-firefly.com>
2019-08-02 16:17:42 +0800

    media: i2c: tc35874x: register driver at later init level for proper init
    Before this modification, tc358749 is only functional as a kernel module loaded
    after system boot up.
    
    It is observed that in the driver's probe function, even though the GPIO has
    output high from software, there is no voltage measured. After the driver
    failure, as the kernel run later, the voltage come up again.
    
    The GPIO is not ready to output at this init level. Moving the driver
    registering at later init level solves this.
    
    Change-Id: I5876c48b7e4d67dcd67970a371abb3e43a33b9d4

--- drivers/media/i2c/tc35874x.c
+++ drivers/media/i2c/tc35874x.c
@@ -2230,7 +2230,7 @@ static const struct of_device_id tc35874x_of_match[] = {
 MODULE_DEVICE_TABLE(of, tc35874x_of_match);
 #endif
 
-static struct i2c_driver tc35874x_driver = {
+static struct i2c_driver tc35874x_i2c_driver = {
 	.driver = {
 		.name = TC35874X_NAME,
 		.of_match_table = of_match_ptr(tc35874x_of_match),
@@ -2240,4 +2240,15 @@ static struct i2c_driver tc35874x_driver = {
 	.id_table = tc35874x_id,
 };
 
-module_i2c_driver(tc35874x_driver);
+static int __init sensor_mod_init(void)
+{
+	return i2c_add_driver(&tc35874x_i2c_driver);
+}
+
+static void __exit sensor_mod_exit(void)
+{
+	i2c_del_driver(&tc35874x_i2c_driver);
+}
+
+device_initcall_sync(sensor_mod_init);
+module_exit(sensor_mod_exit);
