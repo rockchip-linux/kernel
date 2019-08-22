7f3e0513977094950b09402cff1fabcfa2054dd0
daijh <djh@t-chip.com.cn>
2019-08-22 14:26:08 +0800

    net: 8211F LED, 1G enable green and yellow blink, 100M only yellow blink
    Change-Id: I046a2365e359fc1d6f87dc07249af42f9a7108d2

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -54,6 +54,8 @@
 #include <linux/reset.h>
 #include <linux/of_mdio.h>
 
+#define RTL_8211F_PHY_ID  0x001cc916
+
 #define	STMMAC_ALIGN(x)		__ALIGN_KERNEL(x, SMP_CACHE_BYTES)
 #define RTL_8201F_PHY_ID  0x001cc816
 /* Module parameters */
@@ -2822,6 +2824,27 @@ static int stmmac_hw_init(struct stmmac_priv *priv)
 	return 0;
 }
 
+
+static int phy_rtl8211f_led_fixup(struct phy_device *phydev)
+{
+	int value;
+    	printk("%s in\n", __func__);
+
+    	value = phy_read(phydev, 31);
+    	phy_write(phydev, 31, 0xd04);
+
+    	mdelay(10);
+    	value = phy_read(phydev, 16);
+    	value =0x2340;
+    	phy_write(phydev, 16, value);
+
+    	mdelay(10);
+    	phy_read(phydev, 31);
+    	phy_write(phydev, 31, 0x00);
+
+    	return 0;
+}
+
 static int phy_rtl8201f_led_fixup(struct phy_device *phydev)
 {
        int value;
@@ -3001,7 +3024,11 @@ int stmmac_dvr_probe(struct device *device,
     ret = phy_register_fixup_for_uid(RTL_8201F_PHY_ID, 0xffffffff, phy_rtl8201f_led_fixup);
     if (ret)
          pr_warn("Cannot register PHY board fixup.\n");
-	
+    
+    ret = phy_register_fixup_for_uid(RTL_8211F_PHY_ID, 0xffffffff, phy_rtl8211f_led_fixup);
+    if (ret)
+         pr_warn("Cannot register 8211f PHY board fixup.\n");
+
 	ret = register_netdev(ndev);
 	if (ret) {
 		netdev_err(priv->dev, "%s: ERROR %i registering the device\n",
