3d42c531ae4872ab9c14305dfabcf51a1504dc14
huangjc <service@t-firefly.com>
2019-09-09 10:23:30 +0800

    arm64:driver: add rtl8723du wifi module(-m) for rk3399-roc-pc-plus

---
+++ drivers/net/wireless/rockchip_wlan/rtl8723du/hal/rtl8723d/usb/rtl8723du_led.c
@@ -0,0 +1,125 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2007 - 2017 Realtek Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ *****************************************************************************/
+#include "rtl8723d_hal.h"
+#ifdef CONFIG_RTW_SW_LED
+
+/*
+ * ================================================================================
+ * LED object.
+ * ================================================================================
+ */
+
+
+/*
+ * ================================================================================
+ * Prototype of protected function.
+ * ================================================================================
+ */
+
+/*
+ * ================================================================================
+ * LED_819xUsb routines.
+ * ================================================================================
+ */
+
+/*
+ * Description:
+ * Turn on LED according to LedPin specified.
+ */
+void
+SwLedOn_8723DU(
+	PADAPTER padapter,
+	PLED_USB pLed
+)
+{
+	u8 LedCfg;
+
+	if (RTW_CANNOT_RUN(padapter))
+		return;
+
+	pLed->bLedOn = _TRUE;
+
+}
+
+
+/*
+ * Description:
+ * Turn off LED according to LedPin specified.
+ */
+void
+SwLedOff_8723DU(
+	PADAPTER padapter,
+	PLED_USB pLed
+)
+{
+	u8 LedCfg;
+
+	if (RTW_CANNOT_RUN(padapter))
+		goto exit;
+
+exit:
+	pLed->bLedOn = _FALSE;
+
+}
+
+/*
+ * ================================================================================
+ * Interface to manipulate LED objects.
+ * ================================================================================
+ */
+
+/*
+ * ================================================================================
+ * Default LED behavior.
+ * ================================================================================
+ */
+
+/*
+ * Description:
+ * Initialize all LED_871x objects.
+ */
+void
+rtl8723du_InitSwLeds(
+	PADAPTER padapter
+)
+{
+	struct led_priv *pledpriv = adapter_to_led(padapter);
+
+	pledpriv->LedControlHandler = LedControlUSB;
+
+	pledpriv->SwLedOn = SwLedOn_8723DU;
+	pledpriv->SwLedOff = SwLedOff_8723DU;
+
+	InitLed(padapter, &(pledpriv->SwLed0), LED_PIN_LED0);
+
+	InitLed(padapter, &(pledpriv->SwLed1), LED_PIN_LED1);
+}
+
+
+/*
+ * Description:
+ * DeInitialize all LED_819xUsb objects.
+ */
+void
+rtl8723du_DeInitSwLeds(
+	PADAPTER padapter
+)
+{
+	struct led_priv *ledpriv = adapter_to_led(padapter);
+
+	DeInitLed(&(ledpriv->SwLed0));
+	DeInitLed(&(ledpriv->SwLed1));
+}
+#endif
