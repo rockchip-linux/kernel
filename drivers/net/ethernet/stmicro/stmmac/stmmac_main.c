73743213bcc197978d59799c8713a56cb9df81a9
luojh <service@t-firefly.com>
2019-09-20 15:52:57 +0800

    Kernel:driver:Get the MAC address from vendor
    Change-Id: I61bd2eefc41a969cbaabd45a8e8f2991c325000e

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -1625,11 +1625,12 @@ static int stmmac_get_hw_features(struct stmmac_priv *priv)
 static void stmmac_check_ether_addr(struct stmmac_priv *priv)
 {
 	if (!is_valid_ether_addr(priv->dev->dev_addr)) {
-		priv->hw->mac->get_umac_addr(priv->hw,
-					     priv->dev->dev_addr, 0);
-		if (likely(priv->plat->get_eth_addr))
+	    if (likely(priv->plat->get_eth_addr))
 			priv->plat->get_eth_addr(priv->plat->bsp_priv,
 				priv->dev->dev_addr);
+		if (!is_valid_ether_addr(priv->dev->dev_addr))
+            priv->hw->mac->get_umac_addr(priv->hw,
+					     priv->dev->dev_addr, 0);
 		if (!is_valid_ether_addr(priv->dev->dev_addr))
 			eth_hw_addr_random(priv->dev);
 		pr_info("%s: device MAC address %pM\n", priv->dev->name,
