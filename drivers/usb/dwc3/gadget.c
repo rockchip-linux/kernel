b4f537c6a63eba7015a60fad3cb4b85a52e52431
William Wu <william.wu@rock-chips.com>
2020-09-16 10:54:34 +0800

    usb: dwc3: gadget: only resize fifo for isoc ep with 1024 maxpacket
    Signed-off-by: William Wu <william.wu@rock-chips.com>
    Change-Id: If28098b07a1d3bf0a35c4ff37ae7f548283191c7

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -800,7 +800,8 @@ static int dwc3_gadget_ep_enable(struct usb_ep *ep,
 	ret = __dwc3_gadget_ep_enable(dep, desc, ep->comp_desc, false, false);
 	spin_unlock_irqrestore(&dwc->lock, flags);
 
-	if (usb_endpoint_xfer_isoc(ep->desc))
+	if (usb_endpoint_xfer_isoc(ep->desc) &&
+	    usb_endpoint_maxp(ep->desc) >= 1024)
 		dwc3_gadget_resize_tx_fifos(dwc);
 
 	return ret;
