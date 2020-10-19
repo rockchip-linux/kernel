faf7e6133364ce576646941a70a5c7da4b30b8f0
John Ogness <john.ogness@linutronix.de>
2020-10-19 22:11:31 +0206

    printk: combine boot_delay_msec() into printk_delay()
    boot_delay_msec() is always called immediately before printk_delay()
    so just combine the two.
    
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1753,8 +1753,10 @@ static void put_sprint_buf(int id, unsigned long flags)
 
 int printk_delay_msec __read_mostly;
 
-static inline void printk_delay(void)
+static inline void printk_delay(int level)
 {
+	boot_delay_msec(level);
+
 	if (unlikely(printk_delay_msec)) {
 		int m = printk_delay_msec;
 
@@ -2090,8 +2092,7 @@ asmlinkage int vprintk_emit(int facility, int level,
 		in_sched = true;
 	}
 
-	boot_delay_msec(level);
-	printk_delay();
+	printk_delay(level);
 
 	/* This stops the holder of console_sem just where we want him */
 	printed_len = vprintk_store(facility, level, dev_info, fmt, args);
