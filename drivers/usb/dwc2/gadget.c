ad3483c9b245765be36c697741bd56a519aaa1e0
William Wu <william.wu@rock-chips.com>
2021-03-19 17:40:21 +0800

    usb: dwc2: gadget: avoid stop disabled interrupt ep
    When we test RK3308 USB gadget with UAC + HID composite
    function, connect to PC USB Host, and do hotplug test.
    The HID may fail to do ep in transfer. It's because that
    when USB disconnect, the dwc2 call dwc2_hsotg_ep_disable()
    to disable all enabled endpoints. However, for HID in ep,
    the dwc2 call dwc2_hsotg_ep_stop_xfr() to stop transfer
    even if the HID in ep has been disabled. This cause HID
    in ep transfer fail when USB connect next time.
    
    Fixes: 0ba984903508 ("usb: dwc2: gadget: stop isoc ep in transfer when disable ep")
    Signed-off-by: William Wu <william.wu@rock-chips.com>
    Change-Id: I316410ec8ff30cf5e298cfcad327456dbae4e970

--- drivers/usb/dwc2/gadget.c
+++ drivers/usb/dwc2/gadget.c
@@ -3330,7 +3330,8 @@ static int dwc2_hsotg_ep_disable(struct usb_ep *ep)
 	ctrl = dwc2_readl(hsotg->regs + epctrl_reg);
 
 	/* stop isoc ep in transfer even if the ep is disabled */
-	if ((ctrl & DXEPCTL_EPENA) || (dir_in && (ctrl & DXEPCTL_EPTYPE_ISO)))
+	if ((ctrl & DXEPCTL_EPENA) ||
+	    (dir_in && ((ctrl & DXEPCTL_EPTYPE_MASK) == DXEPCTL_EPTYPE_ISO)))
 		dwc2_hsotg_ep_stop_xfr(hsotg, hs_ep);
 
 	ctrl &= ~DXEPCTL_EPENA;
