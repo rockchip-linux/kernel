0b45f93eae69d8c43863d610553651f837490fd9
Vincent Whitchurch <vincent.whitchurch@axis.com>
2021-04-23 13:45:24 +0200

    regulator: core: Fix off_on_delay handling
    commit a8ce7bd89689997537dd22dcbced46cf23dc19da upstream.
    
    Signed-off-by: Vincent Whitchurch <vincent.whitchurch@axis.com>
    Link: https://lore.kernel.org/r/20210423114524.26414-1-vincent.whitchurch@axis.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/regulator/driver.h
+++ include/linux/regulator/driver.h
@@ -477,7 +477,7 @@ struct regulator_dev {
 	unsigned int is_switch:1;
 
 	/* time when this regulator was disabled last time */
-	unsigned long last_off_jiffy;
+	ktime_t last_off;
 
 	ANDROID_KABI_RESERVE(1);
 };
