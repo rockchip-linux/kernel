6dd0792bf246517f6d02541d4f97f230e4ab15c5
Sandy Huang <hjc@rock-chips.com>
2021-05-27 15:21:04 +0800

    drm/rockchip: vop2: make sure layer sel is take effect when it's updated
    when vp0 and vp1 indenpendent config layer_sel register, this register take effect
    time is prone to error, so we add the following measures to workaround this issue:
    
    1. Add commit_lock to make sure vp0 and vp1 config register is mutually exclusive;
    2. Make sure layer sel register is take effect when it's update.
    
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>
    Change-Id: Ief832e2bf7e18567f4ea663843c77f0afbd21cf7

--- drivers/gpu/drm/rockchip/rockchip_drm_fb.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_fb.c
@@ -424,7 +424,9 @@ rockchip_atomic_commit_complete(struct rockchip_atomic_commit *commit)
 		rockchip_dmcfreq_vop_bandwidth_update(prv->devfreq, bandwidth,
 						      plane_num);
 
+	mutex_lock(&prv->commit_lock);
 	drm_atomic_helper_commit_planes(dev, state, true);
+	mutex_unlock(&prv->commit_lock);
 
 	rockchip_drm_psr_inhibit_put_state(state);
 
