c75e213e1718ceaacea403e7b64447d04342df53
Shunqing Chen <csq@rock-chips.com>
2021-05-27 19:39:09 +0800

    drm: rockchip: rk618: hpd io pull down
    Signed-off-by: Shunqing Chen <csq@rock-chips.com>
    Change-Id: I309bc695dbd94281880ce934415c1864c5abc8c8

--- drivers/gpu/drm/rockchip/rk618/rk618_hdmi.c
+++ drivers/gpu/drm/rockchip/rk618/rk618_hdmi.c
@@ -1512,6 +1512,9 @@ static int rk618_hdmi_probe(struct platform_device *pdev)
 		return PTR_ERR(hdmi->clock);
 	}
 
+	/* hpd io pull down */
+	regmap_write(rk618->regmap, RK618_IO_CON0, HDMI_IO_PULL_UP_DISABLE);
+
 	rk618_hdmi_reset(hdmi);
 
 	hdmi->ddc = rk618_hdmi_i2c_adapter(hdmi);
