62df34992b2abca7aefb5b01d005d38b72fd23b9
Tao Huang <huangtao@rock-chips.com>
2021-06-10 18:36:21 +0800

    Revert "UPSTREAM: dma-buf: Fix SET_NAME ioctl uapi"
    This reverts commit 97fd8955415750cf47f4b2b799fd0527c848d2d0.
    
    Relpaced by commit 06931a5d14e2 ("UPSTREAM: dma-buf: Fix SET_NAME ioctl uapi").
    
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: I8623aa664bca0c41967a14b7f2247927c0b26151

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -442,8 +442,7 @@ static long dma_buf_ioctl(struct file *file,
 
 		return ret;
 
-	case DMA_BUF_SET_NAME_A:
-	case DMA_BUF_SET_NAME_B:
+	case DMA_BUF_SET_NAME:
 		return dma_buf_set_name(dmabuf, (const char __user *)arg);
 
 	case DMA_BUF_IOCTL_SYNC_PARTIAL:
