ffbc5b745c72892af85c41b758ca77d6a59e0aeb
Algea Cao <algea.cao@rock-chips.com>
2021-07-02 15:22:55 +0800

    drm/rockchip: dw_hdmi: Add property to show whether sink is DVI
    Add property output_type_capacity:
    enums: DVI=0 HDMI=1
    
    Signed-off-by: Algea Cao <algea.cao@rock-chips.com>
    Change-Id: Iad09b386b55c52e21b01f98e81fadfd5aa1a42d3

--- include/drm/bridge/dw_hdmi.h
+++ include/drm/bridge/dw_hdmi.h
@@ -219,5 +219,6 @@ void dw_hdmi_phy_setup_hpd(struct dw_hdmi *hdmi, void *data);
 void dw_hdmi_set_quant_range(struct dw_hdmi *hdmi);
 void dw_hdmi_set_output_type(struct dw_hdmi *hdmi, u64 val);
 bool dw_hdmi_get_output_whether_hdmi(struct dw_hdmi *hdmi);
+int dw_hdmi_get_output_type_cap(struct dw_hdmi *hdmi);
 
 #endif /* __IMX_HDMI_H__ */
