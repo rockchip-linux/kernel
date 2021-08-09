54e366cc7bd755f2400900f8a0491a4fdf105b8e
Greg Kroah-Hartman <gregkh@google.com>
2021-08-09 16:10:59 +0200

    Revert "bdi: add a ->dev_name field to struct backing_dev_info"
    This reverts commit d6c434ae9d3b5cd1fcb8e3aa28172f3d79d81641 which is
    commit 6bd87eec23cbc9ed222bed0f5b5b02bf300e9a8d upstream.
    
    It breaks the Android KABI and is not needed in this branch at this
    point in time.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I69a71f7ae3dca4c5bec52b31dcc082ad1c343220

--- mm/backing-dev.c
+++ mm/backing-dev.c
@@ -880,8 +880,7 @@ int bdi_register_va(struct backing_dev_info *bdi, const char *fmt, va_list args)
 	if (bdi->dev)	/* The driver needs to use separate queues per device */
 		return 0;
 
-	vsnprintf(bdi->dev_name, sizeof(bdi->dev_name), fmt, args);
-	dev = device_create(bdi_class, NULL, MKDEV(0, 0), bdi, bdi->dev_name);
+	dev = device_create_vargs(bdi_class, NULL, MKDEV(0, 0), bdi, fmt, args);
 	if (IS_ERR(dev))
 		return PTR_ERR(dev);
 
@@ -981,7 +980,7 @@ const char *bdi_dev_name(struct backing_dev_info *bdi)
 {
 	if (!bdi || !bdi->dev)
 		return bdi_unknown_name;
-	return bdi->dev_name;
+	return dev_name(bdi->dev);
 }
 EXPORT_SYMBOL_GPL(bdi_dev_name);
 
