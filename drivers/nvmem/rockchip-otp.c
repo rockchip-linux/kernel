e3f1c67586ab2b35c74d10d9f83e980bf38ed69f
Finley Xiao <finley.xiao@rock-chips.com>
2021-08-19 16:24:22 +0800

    nvmem: rockchip-otp: build depends on CPU config
    This optimizes the size of rockchip-otp.o, details as follows.
    
    ./scripts/bloat-o-meter rockchip-otp-old.o rockchip-otp.o
    add/remove: 0/9 grow/shrink: 0/1 up/down: 0/-2224 (-2224)
    Function                                     old     new   delta
    px30_otp_clocks                               12       -     -12
    rk3568_otp_clocks                             16       -     -16
    rk3568_data                                   24       -     -24
    px30_data                                     24       -     -24
    rockchip_otp_reset                           128       -    -128
    px30_otp_ecc_enable                          164       -    -164
    px30_otp_wait_status                         176       -    -176
    px30_otp_read                                416       -    -416
    rockchip_otp_match                           980     392    -588
    rk3568_otp_read                              676       -    -676
    Total: Before=4721, After=2497, chg -47.11%
    
    Change-Id: I960a51e826a17112d822daa2b77470e482e729d7
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>

--- drivers/nvmem/rockchip-otp.c
+++ drivers/nvmem/rockchip-otp.c
@@ -529,22 +529,30 @@ static const struct rockchip_data rv1126_data = {
 };
 
 static const struct of_device_id rockchip_otp_match[] = {
+#ifdef CONFIG_CPU_PX30
 	{
 		.compatible = "rockchip,px30-otp",
 		.data = (void *)&px30_data,
 	},
+#endif
+#ifdef CONFIG_CPU_RK3308
 	{
 		.compatible = "rockchip,rk3308-otp",
 		.data = (void *)&px30_data,
 	},
+#endif
+#ifdef CONFIG_CPU_RK3568
 	{
 		.compatible = "rockchip,rk3568-otp",
 		.data = (void *)&rk3568_data,
 	},
+#endif
+#ifdef CONFIG_CPU_RV1126
 	{
 		.compatible = "rockchip,rv1126-otp",
 		.data = (void *)&rv1126_data,
 	},
+#endif
 	{ /* sentinel */ },
 };
 MODULE_DEVICE_TABLE(of, rockchip_otp_match);
