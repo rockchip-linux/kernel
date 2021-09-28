bb71dc4ba422355d7813c8d0257d5d769d527154
William Wu <william.wu@rock-chips.com>
2021-09-28 20:21:17 +0800

    usb: quirks: add quirk for MT7668 WIFI with broken LPM
    The MT7668 WIFI/BT can't be detected by xHCI controller
    on Rockchip platforms when the LPM is enabled.
    
    This patch adds an USB_QUIRK_NO_LPM quirk for this device.
    
    Signed-off-by: William Wu <william.wu@rock-chips.com>
    Change-Id: Iacaf6d050ece06e09c2d286206860c2214c52661

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -237,6 +237,9 @@ static const struct usb_device_id usb_quirk_list[] = {
 	{ USB_DEVICE(0x0c45, 0x64ab), .driver_info = USB_QUIRK_AUTO_SUSPEND },
 	{ USB_DEVICE(0x0c45, 0x64ac), .driver_info = USB_QUIRK_AUTO_SUSPEND },
 
+	/* MT7668 WIFI/BT */
+	{ USB_DEVICE(0x0e8d, 0x7668), .driver_info = USB_QUIRK_NO_LPM },
+
 	/* Action Semiconductor flash disk */
 	{ USB_DEVICE(0x10d6, 0x2200), .driver_info =
 			USB_QUIRK_STRING_FETCH_255 },
