09145f10d94216fbe7dc77910fa11caf8266b728
Sandy Huang <hjc@rock-chips.com>
2022-03-07 17:21:55 +0800

    drm/rockchip: vop: init win axi read id for px30
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>
    Change-Id: Ic2752ad9472b2b5658a24c18525947da621f367c

--- drivers/gpu/drm/rockchip/rockchip_vop_reg.c
+++ drivers/gpu/drm/rockchip/rockchip_vop_reg.c
@@ -1352,6 +1352,7 @@ static const struct vop_win_phy rk3366_lit_win0_data = {
 	.global_alpha_val = VOP_REG(RK3366_LIT_WIN0_ALPHA_CTRL, 0xff, 4),
 	.key_color = VOP_REG(RK3366_LIT_WIN0_COLOR_KEY, 0xffffff, 0),
 	.key_en = VOP_REG(RK3366_LIT_WIN0_COLOR_KEY, 0x1, 24),
+	.channel = VOP_REG(RK3366_LIT_WIN0_CTRL0, 0xff, 12),
 };
 
 static const struct vop_win_phy rk3366_lit_win1_data = {
@@ -1372,6 +1373,7 @@ static const struct vop_win_phy rk3366_lit_win1_data = {
 	.global_alpha_val = VOP_REG(RK3366_LIT_WIN1_ALPHA_CTRL, 0xff, 4),
 	.key_color = VOP_REG(RK3366_LIT_WIN1_COLOR_KEY, 0xffffff, 0),
 	.key_en = VOP_REG(RK3366_LIT_WIN1_COLOR_KEY, 0x1, 24),
+	.channel = VOP_REG(RK3366_LIT_WIN1_CTRL1, 0xf, 8),
 };
 
 static const struct vop_win_data rk3366_vop_lit_win_data[] = {
