25b3549a4c46e10f4dcfe627a5bda1f1a1f7198d
Kever Yang <kever.yang@rock-chips.com>
2022-05-13 09:54:12 +0800

    phy: rockchip-snps-pcie3: update fw when init
    This fw fix some RX issue:
    1. connect detect error;
    2. transfer error in ssd huge data write(more than 10GB).
    
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>
    Change-Id: If9b8d7fbe4414ae4d28ff6bbd4415d88b582113d

--- drivers/phy/rockchip/phy-rockchip-snps-pcie3.c
+++ drivers/phy/rockchip/phy-rockchip-snps-pcie3.c
@@ -19,6 +19,7 @@
 #include <dt-bindings/phy/phy.h>
 
 #define GRF_PCIE30PHY_CON1 0x4
+#define GRF_PCIE30PHY_CON4 0x10
 #define GRF_PCIE30PHY_CON6 0x18
 #define GRF_PCIE30PHY_CON9 0x24
 #define GRF_PCIE30PHY_STATUS0 0x80
@@ -59,10 +60,15 @@ static int rockchip_p3phy_set_mode(struct phy *phy, enum phy_mode mode)
 	return 0;
 }
 
+static const u16 phy_fw[] = {
+	#include "phy-rockchip-snps-pcie3.fw"
+};
+
 static int rochchip_p3phy_init(struct phy *phy)
 {
 	struct rockchip_p3phy_priv *priv = phy_get_drvdata(phy);
-	int ret;
+	int i;
+	int ret = 0;
 	u32 reg;
 
 	ret = clk_prepare_enable(priv->ref_clk_m);
@@ -91,6 +97,10 @@ static int rochchip_p3phy_init(struct phy *phy)
 			     (0x1 << 15) | (0x1 << 31));
 	}
 
+	regmap_write(priv->phy_grf, GRF_PCIE30PHY_CON4,
+		     (0x0 << 14) | (0x1 << (14 + 16))); //sdram_ld_done
+	regmap_write(priv->phy_grf, GRF_PCIE30PHY_CON4,
+		     (0x0 << 13) | (0x1 << (13 + 16))); //sdram_bypass
 	reset_control_deassert(priv->p30phy);
 
 	ret = regmap_read_poll_timeout(priv->phy_grf,
@@ -102,6 +112,17 @@ static int rochchip_p3phy_init(struct phy *phy)
 		       __func__, reg);
 		goto err_pclk;
 	}
+
+	regmap_write(priv->phy_grf, GRF_PCIE30PHY_CON9,
+		     (0x3 << 8) | (0x3 << (8 + 16))); //map to access sram
+	for (i = 0; i < ARRAY_SIZE(phy_fw); i++)
+		writel(phy_fw[i], priv->mmio + (i<<2));
+	pr_info("snps pcie3phy FW update! size %ld\n", ARRAY_SIZE(phy_fw));
+
+	regmap_write(priv->phy_grf, GRF_PCIE30PHY_CON9,
+		     (0x0 << 8) | (0x3 << (8 + 16)));
+	regmap_write(priv->phy_grf, GRF_PCIE30PHY_CON4,
+		     (0x1 << 14) | (0x1 << (14 + 16))); //sdram_ld_done
 
 	return 0;
 err_pclk:
