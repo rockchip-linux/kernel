412baa96f95cfae899a9ffb9f185c4c9d023299e
Sandy Huang <hjc@rock-chips.com>
2022-07-26 20:56:21 +0800

    drm/rockchip: vop2: add support soft te for mipi cmd mode
    some product will use gpio to instead of TE, so we add soft TE for all
    vp.
    
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>
    Change-Id: Idb2d8b6a63c17308519761ae986f853b2baf0bc4

--- drivers/gpu/drm/rockchip/rockchip_drm_drv.h
+++ drivers/gpu/drm/rockchip/rockchip_drm_drv.h
@@ -130,6 +130,12 @@ struct rockchip_crtc_state {
 	 * (3) edp psr mode
 	 */
 	bool hold_mode;
+	/**
+	 * when enable soft_te, use gpio irq to triggle new fs,
+	 * otherwise use hardware te
+	 */
+	bool soft_te;
+
 	struct drm_tv_connector_state *tv_state;
 	int left_margin;
 	int right_margin;
